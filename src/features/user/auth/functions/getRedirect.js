009144b4686404a638182c0e4754c9c77b533d97
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2021-10-04 14:32:52 +0900

    確認メール 不具合 修正

--- src/features/user/auth/functions/getRedirect.js
+++ src/features/user/auth/functions/getRedirect.js
@@ -1,17 +1,32 @@
 import { auth } from "../../../../firebase";
 import * as userSlice from "../../userSlice";
 
-export const getRedirect = ({ dispatch, email, verified }) => {
+export const getRedirect = ({ dispatch }) => {
   auth
     .getRedirectResult()
     .then((result) => {
-      if (result.credential) {
+      if (result.credential && result.user.emailVerified) {
         dispatch(
           userSlice.handleAnnounce({
             type: "success",
             text: "認証されました",
           })
         );
+      }
+
+      if (!result.user.emailVerified) {
+        auth.currentUser
+          .sendEmailVerification({
+            url: "https://ses-hub.app/login",
+          })
+          .catch((e) => {
+            dispatch(
+              userSlice.handleAnnounce({
+                type: "error",
+                text: "再度時間をおいてください",
+              })
+            );
+          });
       }
     })
     .catch((e) => {
