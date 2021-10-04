1f503fa7f27dbc7d42ee4a98ae5615b896803a4c
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2021-10-04 13:31:24 +0900

    ユーザーページ プラン情報 表示

---
+++ src/features/user/layouts/main/components/profile/components/company/components/Payment.jsx
@@ -0,0 +1,22 @@
+import styles from "../../../Profile.module.scss";
+
+export const Payment = ({ user }) => {
+  return (
+    <div
+      className={`${styles.profile_payment} ${
+        user?.payment?.status === "active"
+          ? styles.profile_payment_active
+          : user?.payment?.status === "trialing" &&
+            styles.profile_payment_trialing
+      }`}
+    >
+      <span className={styles.profile_payment_txt}>
+        {user?.payment?.status === "active"
+          ? "レギュラー"
+          : user?.payment?.status === "trialing"
+          ? "レギュラー(フリートライアル)"
+          : "リミテッド"}
+      </span>
+    </div>
+  );
+};
