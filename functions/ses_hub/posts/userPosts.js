f2f400f59e7143870900c269667d90dc3fdf0a32
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2021-10-04 17:24:31 +0900

    フォローユーザー 表示されない不具合 修正

--- functions/ses_hub/posts/userPosts.js
+++ functions/ses_hub/posts/userPosts.js
@@ -38,174 +38,194 @@ exports.userPosts = functions
 
     const index = algolia.initIndex(data.index);
 
-    const hit = {
-      currentPage: data.page ? data.page : 0,
-    };
+    const hitsPerPage = 50;
 
-    const posts = await index
-      .search(
-        !data.uids ? data.uid : "",
-        data.index === "matters" || data.index === "resources"
-          ? {
-              filters:
-                data.uid !== context.auth.uid
-                  ? "display:public"
-                  : data.display && data.status
-                  ? `display:${data.display} AND status:${data.status}`
-                  : data.display
-                  ? `display:${data.display}`
-                  : data.status
-                  ? `status:${data.status}`
-                  : "",
-              page: hit.currentPage,
-            }
-          : data.index === "companys" && {
-              similarQuery: data.uids.join(" "),
-              filters: "status:enable",
-              page: hit.currentPage,
-            }
-      )
-      .then((result) => {
-        hit.posts = result.nbHits;
-        hit.pages = result.nbPages;
-        return result.hits.map((hit) =>
-          data.index === "matters" && hit.uid === context.auth.uid
-            ? {
-                display: hit.display,
-                objectID: hit.objectID,
-                title: hit.title,
-                position: hit.position,
-                body: hit.body,
-                location: hit.location,
-                period: hit.period,
-                costs: hit.costs,
-                adjustment: hit.adjustment,
-                times: hit.times,
-                handles: hit.handles,
-                tools: hit.tools,
-                requires: hit.requires,
-                prefers: hit.prefers,
-                interviews: hit.interviews,
-                remote: hit.remote,
-                distribution: hit.distribution,
-                span: hit.span,
-                note: hit.note,
-                status: hit.status,
-                memo: hit.memo,
-                uid: hit.uid,
-                createAt: hit.createAt,
-                updateAt: hit.updateAt,
-              }
-            : data.index === "matters" && status
-            ? {
-                objectID: hit.objectID,
-                title: hit.title,
-                position: hit.position,
-                body: hit.body,
-                location: hit.location,
-                period: hit.period,
-                costs:
-                  hit.costs.display === "public"
-                    ? {
-                        display: hit.costs.display,
-                        min: hit.costs.min,
-                        max: hit.costs.max,
-                      }
-                    : {
-                        display: hit.costs.display,
-                        type: hit.costs.type,
-                      },
-                adjustment: hit.adjustment,
-                times: hit.times,
-                handles: hit.handles,
-                tools: hit.tools,
-                requires: hit.requires,
-                prefers: hit.prefers,
-                interviews: hit.interviews,
-                remote: hit.remote,
-                distribution: hit.distribution,
-                span: hit.span,
-                note: hit.note,
-                uid: hit.uid,
-                createAt: hit.createAt,
-                updateAt: hit.updateAt,
-              }
-            : data.index === "resources" && hit.uid === context.auth.uid
-            ? {
-                display: hit.display,
-                objectID: hit.objectID,
-                roman: hit.roman,
-                position: hit.position,
-                sex: hit.sex,
-                age: hit.age,
-                body: hit.body,
-                belong: hit.belong,
-                station: hit.station,
-                period: hit.period,
-                costs: hit.costs,
-                handles: hit.handles,
-                tools: hit.tools,
-                skills: hit.skills,
-                parallel: hit.parallel,
-                note: hit.note,
-                status: hit.status,
-                memo: hit.memo,
-                uid: hit.uid,
-                createAt: hit.createAt,
-                updateAt: hit.updateAt,
-              }
-            : data.index === "resources" && status
-            ? {
-                objectID: hit.objectID,
-                roman: {
-                  firstName: hit.roman.firstName.substring(0, 1),
-                  lastName: hit.roman.lastName.substring(0, 1),
-                },
-                position: hit.position,
-                sex: hit.sex,
-                age: hit.age,
-                body: hit.body,
-                belong: hit.belong,
-                station: hit.station,
-                period: hit.period,
-                costs:
-                  hit.costs.display === "public"
-                    ? {
-                        display: hit.costs.display,
-                        min: hit.costs.min,
-                        max: hit.costs.max,
-                      }
-                    : {
-                        display: hit.costs.display,
-                        type: hit.costs.type,
-                      },
-                handles: hit.handles,
-                tools: hit.tools,
-                skills: hit.skills,
-                parallel: hit.parallel,
-                note: hit.note,
-                uid: hit.uid,
-                createAt: hit.createAt,
-                updateAt: hit.updateAt,
-              }
-            : data.index === "companys" &&
-              status && {
-                uid: hit.objectID,
-                profile: {
-                  name: hit.name,
-                  person: hit.person,
-                  body: hit.body,
-                },
-              }
-        );
-      })
-      .catch((e) => {
-        throw new functions.https.HttpsError(
-          "not-found",
-          "投稿の取得に失敗しました",
-          "algolia"
-        );
-      });
+    const hit = !data.uids
+      ? {
+          currentPage: data.page ? data.page : 0,
+        }
+      : {
+          posts: data.uids.length,
+          pages: Math.ceil(data.uids.length / 50),
+          currentPage: data.page ? data.page : 0,
+        };
+
+    const posts = !data.uids
+      ? await index
+          .search(data.uid, {
+            filters:
+              data.uid !== context.auth.uid
+                ? "display:public"
+                : data.display && data.status
+                ? `display:${data.display} AND status:${data.status}`
+                : data.display
+                ? `display:${data.display}`
+                : data.status
+                ? `status:${data.status}`
+                : "",
+            page: hit.currentPage,
+          })
+          .then((result) => {
+            hit.posts = result.nbHits;
+            hit.pages = result.nbPages;
+            return result.hits.map((hit) =>
+              data.index === "matters" && hit.uid === context.auth.uid
+                ? {
+                    display: hit.display,
+                    objectID: hit.objectID,
+                    title: hit.title,
+                    position: hit.position,
+                    body: hit.body,
+                    location: hit.location,
+                    period: hit.period,
+                    costs: hit.costs,
+                    adjustment: hit.adjustment,
+                    times: hit.times,
+                    handles: hit.handles,
+                    tools: hit.tools,
+                    requires: hit.requires,
+                    prefers: hit.prefers,
+                    interviews: hit.interviews,
+                    remote: hit.remote,
+                    distribution: hit.distribution,
+                    span: hit.span,
+                    note: hit.note,
+                    status: hit.status,
+                    memo: hit.memo,
+                    uid: hit.uid,
+                    createAt: hit.createAt,
+                    updateAt: hit.updateAt,
+                  }
+                : data.index === "matters" && status
+                ? {
+                    objectID: hit.objectID,
+                    title: hit.title,
+                    position: hit.position,
+                    body: hit.body,
+                    location: hit.location,
+                    period: hit.period,
+                    costs:
+                      hit.costs.display === "public"
+                        ? {
+                            display: hit.costs.display,
+                            min: hit.costs.min,
+                            max: hit.costs.max,
+                          }
+                        : {
+                            display: hit.costs.display,
+                            type: hit.costs.type,
+                          },
+                    adjustment: hit.adjustment,
+                    times: hit.times,
+                    handles: hit.handles,
+                    tools: hit.tools,
+                    requires: hit.requires,
+                    prefers: hit.prefers,
+                    interviews: hit.interviews,
+                    remote: hit.remote,
+                    distribution: hit.distribution,
+                    span: hit.span,
+                    note: hit.note,
+                    uid: hit.uid,
+                    createAt: hit.createAt,
+                    updateAt: hit.updateAt,
+                  }
+                : data.index === "resources" && hit.uid === context.auth.uid
+                ? {
+                    display: hit.display,
+                    objectID: hit.objectID,
+                    roman: hit.roman,
+                    position: hit.position,
+                    sex: hit.sex,
+                    age: hit.age,
+                    body: hit.body,
+                    belong: hit.belong,
+                    station: hit.station,
+                    period: hit.period,
+                    costs: hit.costs,
+                    handles: hit.handles,
+                    tools: hit.tools,
+                    skills: hit.skills,
+                    parallel: hit.parallel,
+                    note: hit.note,
+                    status: hit.status,
+                    memo: hit.memo,
+                    uid: hit.uid,
+                    createAt: hit.createAt,
+                    updateAt: hit.updateAt,
+                  }
+                : data.index === "resources" &&
+                  status && {
+                    objectID: hit.objectID,
+                    roman: {
+                      firstName: hit.roman.firstName.substring(0, 1),
+                      lastName: hit.roman.lastName.substring(0, 1),
+                    },
+                    position: hit.position,
+                    sex: hit.sex,
+                    age: hit.age,
+                    body: hit.body,
+                    belong: hit.belong,
+                    station: hit.station,
+                    period: hit.period,
+                    costs:
+                      hit.costs.display === "public"
+                        ? {
+                            display: hit.costs.display,
+                            min: hit.costs.min,
+                            max: hit.costs.max,
+                          }
+                        : {
+                            display: hit.costs.display,
+                            type: hit.costs.type,
+                          },
+                    handles: hit.handles,
+                    tools: hit.tools,
+                    skills: hit.skills,
+                    parallel: hit.parallel,
+                    note: hit.note,
+                    uid: hit.uid,
+                    createAt: hit.createAt,
+                    updateAt: hit.updateAt,
+                  }
+            );
+          })
+          .catch((e) => {
+            throw new functions.https.HttpsError(
+              "not-found",
+              "投稿の取得に失敗しました",
+              "algolia"
+            );
+          })
+      : await index
+          .getObjects(
+            data.uids.slice(
+              hit.currentPage * hitsPerPage,
+              hitsPerPage * (hit.currentPage + 1)
+            )
+          )
+          .then(({ results }) => {
+            return results.map(
+              (hit) =>
+                hit &&
+                data.index === "companys" &&
+                status && {
+                  uid: hit.objectID,
+                  profile: {
+                    name: hit.name,
+                    person: hit.person,
+                    body: hit.body,
+                  },
+                }
+            );
+          })
+          .catch((e) => {
+            throw new functions.https.HttpsError(
+              "not-found",
+              "投稿の取得に失敗しました",
+              "algolia"
+            );
+          });
 
     if (data.index === "companys") {
       for (let i = 0; i < posts.length; i++) {
