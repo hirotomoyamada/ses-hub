605bc5ca4abe86a48e4d20e4c4490f776e8f79a7
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2021-10-06 14:28:46 +0900

    関数 小分け

--- functions/ses_hub/posts/promotionPosts.js
+++ functions/ses_hub/posts/promotionPosts.js
@@ -3,6 +3,8 @@ const algolia = require("../../algolia").algolia;
 const location = require("../../firebase").location;
 const runtime = require("../../firebase").runtime;
 
+const fetch = require("./fetch/fetch");
+
 exports.promotionPosts = functions
   .region(location)
   .runWith(runtime)
@@ -17,56 +19,9 @@ exports.promotionPosts = functions
       .then((result) => {
         return result.hits.map((hit) =>
           data.index === "matters"
-            ? {
-                objectID: hit.objectID,
-                title: hit.title,
-                position: hit.position,
-                body: hit.body,
-                location: hit.location,
-                costs:
-                  hit.costs.display === "public"
-                    ? {
-                        display: hit.costs.display,
-                        min: hit.costs.min,
-                        max: hit.costs.max,
-                      }
-                    : {
-                        display: hit.costs.display,
-                        type: hit.costs.type,
-                      },
-                adjustment: hit.adjustment,
-                times: hit.times,
-                handles: hit.handles,
-                remote: hit.remote,
-                uid: hit.uid,
-                createAt: hit.createAt,
-              }
-            : data.index === "resources" && {
-                objectID: hit.objectID,
-                roman: {
-                  firstName: hit.roman.firstName.substring(0, 1),
-                  lastName: hit.roman.lastName.substring(0, 1),
-                },
-                position: hit.position,
-                body: hit.body,
-                belong: hit.belong,
-                station: hit.station,
-                period: hit.period,
-                costs:
-                  hit.costs.display === "public"
-                    ? {
-                        display: hit.costs.display,
-                        min: hit.costs.min,
-                        max: hit.costs.max,
-                      }
-                    : {
-                        display: hit.costs.display,
-                        type: hit.costs.type,
-                      },
-                handles: hit.handles,
-                uid: hit.uid,
-                createAt: hit.createAt,
-              }
+            ? fetch.matters({ hit: hit, promotion: true })
+            : data.index === "resources" &&
+              fetch.resources({ hit: hit, promotion: true })
         );
       })
       .catch((e) => {
