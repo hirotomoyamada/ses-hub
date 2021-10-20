91e9c9705d8776756cd7c9cecc52bd478be522fa
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2021-10-20 14:33:21 +0900

    プロフィールが未記入の場合処置 追加

--- src/features/post/item/components/user/main/components/Body.jsx
+++ src/features/post/item/components/user/main/components/Body.jsx
@@ -2,8 +2,18 @@ import styles from "../Main.module.scss";
 
 export const Body = ({ post }) => {
   return (
-    <div className={styles.main_body}>
-      <p className={styles.main_body_txt}>{post?.profile?.body}</p>
+    <div
+      className={`${styles.main_body} ${
+        !post?.profile?.body && styles.main_body_none
+      }`}
+    >
+      <p
+        className={`${styles.main_body_txt} ${
+          !post?.profile?.body && styles.main_body_txt_none
+        }`}
+      >
+        {post?.profile?.body ? post?.profile?.body : "まだ記入がありません"}
+      </p>
     </div>
   );
 };
