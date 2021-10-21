0f33fd46205809f8bc9088df8bfa50889ead9dfc
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2021-10-21 21:38:30 +0900

    不具合 修正

--- functions/freelance_direct/users/fetch/persons.js
+++ functions/freelance_direct/users/fetch/persons.js
@@ -1,23 +1,21 @@
 exports.persons = ({ context, doc, data }) => {
-  if (data) {
-    return {
-      uid: context.auth.uid,
-      icon: doc.data().icon,
-      cover: doc.data().cover,
-      provider: data
-        ? data.providerData.map((provider) => provider.providerId)
-        : doc.data().provider,
-      profile: doc.data().profile,
-      agree: doc.data().agree,
-      likes: doc.data().likes,
-      entries: doc.data().entries,
-      requests: doc.data().requests,
-      resume: doc.data().resume,
-      follows: doc.data().follows,
-      home: doc.data().home,
-      history: doc.data().history,
-      createAt: doc.data().createAt,
-      updateAt: doc.data().updateAt,
-    };
-  }
+  return {
+    uid: context.auth.uid,
+    icon: doc.data().icon,
+    cover: doc.data().cover,
+    provider: data
+      ? data.providerData.map((provider) => provider.providerId)
+      : doc.data().provider,
+    profile: doc.data().profile,
+    agree: doc.data().agree,
+    likes: doc.data().likes,
+    entries: doc.data().entries,
+    requests: doc.data().requests,
+    resume: doc.data().resume,
+    follows: doc.data().follows,
+    home: doc.data().home,
+    history: doc.data().history,
+    createAt: doc.data().createAt,
+    updateAt: doc.data().updateAt,
+  };
 };
