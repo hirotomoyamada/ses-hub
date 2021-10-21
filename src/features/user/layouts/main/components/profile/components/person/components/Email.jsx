00db01a000466777e3bf0e09af01acd0ce9a9ae9
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2021-10-21 17:34:58 +0900

    ダミー情報をバック側に移動

--- src/features/user/layouts/main/components/profile/components/person/components/Email.jsx
+++ src/features/user/layouts/main/components/profile/components/person/components/Email.jsx
@@ -4,26 +4,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 
 export const Email = ({ user }) => {
-  const dummy = [
-    "miseruwakeganai@gmail.com",
-    "dameyo-damedame@dame.co.js",
-    "watasi-ha-yamada@yamada.com",
-    "tousen-omedetougozaimasu@tousen.mail",
-    "oreoreoreoreore-sagi@gmail.com",
-  ];
-
   return (
     <div className={styles.profile_field}>
       <FontAwesomeIcon icon={faEnvelope} className={styles.profile_icon} />
       <a
         href={`mailto:${user?.profile?.email}`}
         className={`${styles.profile_link} ${
-          user?.profile?.email === "非公開" && styles.profile_link_dummy
+          user?.request !== "enable" && styles.profile_link_dummy
         }`}
       >
-        {user?.profile?.email !== "非公開"
-          ? user?.profile?.email
-          : dummy[Math.floor(Math.random() * dummy.length)]}
+        {user?.profile?.email}
       </a>
     </div>
   );
