e12a564737c20656fbb2f06dd82cc610dec7d0b5
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2021-10-22 16:23:03 +0900

    コード整形

--- functions/freelance_direct/users/changeEmail.js
+++ functions/freelance_direct/users/changeEmail.js
@@ -13,61 +13,69 @@ exports.changeEmail = functions
   .https.onCall(async (data, context) => {
     await userAuthenticated({ context: context, demo: true });
 
-    const timestamp = Date.now();
-
-    const index = algolia.initIndex("persons");
-
-    await db
-      .collection("persons")
-      .doc(context.auth.uid)
-      .get()
-      .then((doc) => {
-        if (doc.exists) {
-          doc.ref
-            .set(
-              {
-                profile: {
-                  email: data.email,
-                },
-                updateAt: timestamp,
-              },
-              { merge: true }
-            )
-            .catch((e) => {
-              throw new functions.https.HttpsError(
-                "data-loss",
-                "メールアドレスの更新に失敗しました",
-                "firebase"
-              );
-            });
-        }
-      })
-      .catch((e) => {
-        throw new functions.https.HttpsError(
-          "not-found",
-          "ユーザーの取得に失敗しました",
-          "firebase"
-        );
-      });
-
-    await index
-      .partialUpdateObject(
-        {
-          objectID: context.auth.uid,
-          email: data.email,
-          updateAt: timestamp,
-        },
-        {
-          createIfNotExists: true,
-        }
-      )
-      .catch((e) => {
-        throw new functions.https.HttpsError(
-          "data-loss",
-          "メールアドレスの更新に失敗しました",
-          "algolia"
-        );
-      });
+    await addFirestore(context, data);
+    await addAlgolia(context, data);
 
     return;
   });
+
+const addFirestore = async (context, data) => {
+  const timestamp = Date.now();
+
+  await db
+    .collection("persons")
+    .doc(context.auth.uid)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        doc.ref
+          .set(
+            {
+              profile: {
+                email: data.email,
+              },
+              updateAt: timestamp,
+            },
+            { merge: true }
+          )
+          .catch((e) => {
+            throw new functions.https.HttpsError(
+              "data-loss",
+              "メールアドレスの更新に失敗しました",
+              "firebase"
+            );
+          });
+      }
+    })
+    .catch((e) => {
+      throw new functions.https.HttpsError(
+        "not-found",
+        "ユーザーの取得に失敗しました",
+        "firebase"
+      );
+    });
+};
+
+const addAlgolia = async (context, data) => {
+  const index = algolia.initIndex("persons");
+  const timestamp = Date.now();
+
+  await index
+    .partialUpdateObject(
+      {
+        objectID: context.auth.uid,
+        email: data.email,
+        updateAt: timestamp,
+      },
+      {
+        createIfNotExists: true,
+      }
+    )
+    .catch((e) => {
+      throw new functions.https.HttpsError(
+        "data-loss",
+        "メールアドレスの更新に失敗しました",
+        "algolia"
+      );
+    });
+};
