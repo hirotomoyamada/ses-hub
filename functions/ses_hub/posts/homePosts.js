22998bc5687f3207d00103bcb23608e8f0b6a79b
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2021-10-22 15:55:56 +0900

    コード整形

--- functions/ses_hub/posts/homePosts.js
+++ functions/ses_hub/posts/homePosts.js
@@ -14,72 +14,80 @@ exports.homePosts = functions
   .https.onCall(async (data, context) => {
     const status = await userAuthenticated(context);
 
-    const index = algolia.initIndex(data.index);
-    const value = [context.auth.uid, ...data.follows].join(" ");
+    const { posts, hit } = await fetchAlgolia(context, data, status);
 
-    const hit = {
-      currentPage: data.page ? data.page : 0,
-    };
-
-    const posts = await index
-      .search("", {
-        queryLanguages: ["ja", "en"],
-        similarQuery: value,
-        filters: "display:public",
-        page: hit.currentPage,
-      })
-      .then((result) => {
-        hit.posts = result.nbHits;
-        hit.pages = result.nbPages;
-        return result.hits.map((hit) =>
-          hit && data.index === "matters" && hit.uid === context.auth.uid
-            ? fetch.matters({ hit: hit, auth: true })
-            : hit && data.index === "matters" && status
-            ? fetch.matters({ hit: hit })
-            : hit && data.index === "resources" && hit.uid === context.auth.uid
-            ? fetch.resources({ hit: hit, auth: true })
-            : hit &&
-              data.index === "resources" &&
-              status &&
-              fetch.resources({ hit: hit })
-        );
-      })
-      .catch((e) => {
-        throw new functions.https.HttpsError(
-          "not-found",
-          "投稿の取得に失敗しました",
-          "algolia"
-        );
-      });
-
-    if (posts.length) {
-      for (let i = 0; i < posts.length; i++) {
-        posts[i] &&
-          (await db
-            .collection("companys")
-            .doc(posts[i].uid)
-            .get()
-            .then((doc) => {
-              if (doc.exists) {
-                posts[i].user = {
-                  name: doc.data().profile.name,
-                  person: doc.data().profile.person,
-                };
-              } else {
-                posts[i].user = {
-                  person: "不明なメンバー",
-                };
-              }
-            })
-            .catch((e) => {
-              throw new functions.https.HttpsError(
-                "not-found",
-                "ユーザーの取得に失敗しました",
-                "firebase"
-              );
-            }));
-      }
-    }
+    posts.length && (await fetchFiretore(posts));
 
     return { index: data.index, posts: posts, hit: hit };
   });
+
+const fetchAlgolia = async (context, data, status) => {
+  const index = algolia.initIndex(data.index);
+  const value = [context.auth.uid, ...data.follows].join(" ");
+
+  const hit = {
+    currentPage: data.page ? data.page : 0,
+  };
+
+  const posts = await index
+    .search("", {
+      queryLanguages: ["ja", "en"],
+      similarQuery: value,
+      filters: "display:public",
+      page: hit.currentPage,
+    })
+    .then((result) => {
+      hit.posts = result.nbHits;
+      hit.pages = result.nbPages;
+      return result.hits.map((hit) =>
+        hit && data.index === "matters" && hit.uid === context.auth.uid
+          ? fetch.matters({ hit: hit, auth: true })
+          : hit && data.index === "matters" && status
+          ? fetch.matters({ hit: hit })
+          : hit && data.index === "resources" && hit.uid === context.auth.uid
+          ? fetch.resources({ hit: hit, auth: true })
+          : hit &&
+            data.index === "resources" &&
+            status &&
+            fetch.resources({ hit: hit })
+      );
+    })
+    .catch((e) => {
+      throw new functions.https.HttpsError(
+        "not-found",
+        "投稿の取得に失敗しました",
+        "algolia"
+      );
+    });
+
+  return { posts, hit };
+};
+
+const fetchFiretore = async (posts) => {
+  for (let i = 0; i < posts.length; i++) {
+    posts[i] &&
+      (await db
+        .collection("companys")
+        .doc(posts[i].uid)
+        .get()
+        .then((doc) => {
+          if (doc.exists) {
+            posts[i].user = {
+              name: doc.data().profile.name,
+              person: doc.data().profile.person,
+            };
+          } else {
+            posts[i].user = {
+              person: "不明なメンバー",
+            };
+          }
+        })
+        .catch((e) => {
+          throw new functions.https.HttpsError(
+            "not-found",
+            "ユーザーの取得に失敗しました",
+            "firebase"
+          );
+        }));
+  }
+};
--- functions/ses_hub/posts/userPosts.js
+++ functions/ses_hub/posts/userPosts.js
@@ -14,108 +14,126 @@ exports.userPosts = functions
   .https.onCall(async (data, context) => {
     const status = await userAuthenticated(context);
 
-    const index = algolia.initIndex(data.index);
+    const { posts, hit } = !data.uids
+      ? await fetchPosts(context, data, status)
+      : await fetchFollows(data, status);
 
-    const hitsPerPage = 50;
-
-    const hit = !data.uids
-      ? {
-          currentPage: data.page ? data.page : 0,
-        }
-      : {
-          posts: data.uids.length,
-          pages: Math.ceil(data.uids.length / 50),
-          currentPage: data.page ? data.page : 0,
-        };
-
-    const posts = !data.uids
-      ? await index
-          .search(data.uid, {
-            filters:
-              data.uid !== context.auth.uid
-                ? "display:public"
-                : data.display && data.status
-                ? `display:${data.display} AND status:${data.status}`
-                : data.display
-                ? `display:${data.display}`
-                : data.status
-                ? `status:${data.status}`
-                : "",
-            page: hit.currentPage,
-          })
-          .then((result) => {
-            hit.posts = result.nbHits;
-            hit.pages = result.nbPages;
-            return result.hits.map((hit) =>
-              data.index === "matters" && hit.uid === context.auth.uid
-                ? fetch.matters({ hit: hit, auth: true })
-                : data.index === "matters" && status
-                ? fetch.matters({ hit: hit })
-                : data.index === "resources" && hit.uid === context.auth.uid
-                ? fetch.resources({ hit: hit, auth: true })
-                : data.index === "resources" &&
-                  status &&
-                  fetch.resources({ hit: hit })
-            );
-          })
-          .catch((e) => {
-            throw new functions.https.HttpsError(
-              "not-found",
-              "投稿の取得に失敗しました",
-              "algolia"
-            );
-          })
-      : await index
-          .getObjects(
-            data.uids.slice(
-              hit.currentPage * hitsPerPage,
-              hitsPerPage * (hit.currentPage + 1)
-            )
-          )
-          .then(({ results }) => {
-            return results.map(
-              (hit) =>
-                hit &&
-                hit.status === "enable" &&
-                status && {
-                  uid: hit.objectID,
-                  profile: {
-                    name: hit.name,
-                    person: hit.person,
-                    body: hit.body,
-                  },
-                }
-            );
-          })
-          .catch((e) => {
-            throw new functions.https.HttpsError(
-              "not-found",
-              "投稿の取得に失敗しました",
-              "algolia"
-            );
-          });
-
-    if (data.index === "companys") {
-      for (let i = 0; i < posts.length; i++) {
-        posts[i] &&
-          (await db
-            .collection(data.index)
-            .doc(posts[i].uid)
-            .get()
-            .then((doc) => {
-              if (doc.exists) {
-                posts[i].icon = doc.data().icon;
-              }
-            })
-            .catch((e) => {
-              throw new functions.https.HttpsError(
-                "not-found",
-                "ユーザーの取得に失敗しました",
-                "firebase"
-              );
-            }));
-      }
-    }
+    posts.length &&
+      data.index === "companys" &&
+      (await fetchFirestore(data, posts));
 
     return { index: data.index, posts: posts, hit: hit };
   });
+
+const fetchPosts = async (context, data, status) => {
+  const index = algolia.initIndex(data.index);
+
+  const hit = {
+    currentPage: data.page ? data.page : 0,
+  };
+
+  const posts = await index
+    .search(data.uid, {
+      filters:
+        data.uid !== context.auth.uid
+          ? "display:public"
+          : data.display && data.status
+          ? `display:${data.display} AND status:${data.status}`
+          : data.display
+          ? `display:${data.display}`
+          : data.status
+          ? `status:${data.status}`
+          : "",
+      page: hit.currentPage,
+    })
+    .then((result) => {
+      hit.posts = result.nbHits;
+      hit.pages = result.nbPages;
+      return result.hits.map((hit) =>
+        data.index === "matters" && hit.uid === context.auth.uid
+          ? fetch.matters({ hit: hit, auth: true })
+          : data.index === "matters" && status
+          ? fetch.matters({ hit: hit })
+          : data.index === "resources" && hit.uid === context.auth.uid
+          ? fetch.resources({ hit: hit, auth: true })
+          : data.index === "resources" &&
+            status &&
+            fetch.resources({ hit: hit })
+      );
+    })
+    .catch((e) => {
+      throw new functions.https.HttpsError(
+        "not-found",
+        "投稿の取得に失敗しました",
+        "algolia"
+      );
+    });
+
+  return { posts, hit };
+};
+
+const fetchFollows = async (data, status) => {
+  const index = algolia.initIndex(data.index);
+
+  const hitsPerPage = 50;
+
+  const hit = {
+    posts: data.uids.length,
+    pages: Math.ceil(data.uids.length / 50),
+    currentPage: data.page ? data.page : 0,
+  };
+
+  const posts = await index
+    .getObjects(
+      data.uids.slice(
+        hit.currentPage * hitsPerPage,
+        hitsPerPage * (hit.currentPage + 1)
+      )
+    )
+    .then(({ results }) => {
+      return results.map(
+        (hit) =>
+          hit &&
+          hit.status === "enable" &&
+          status && {
+            uid: hit.objectID,
+            profile: {
+              name: hit.name,
+              person: hit.person,
+              body: hit.body,
+            },
+          }
+      );
+    })
+    .catch((e) => {
+      throw new functions.https.HttpsError(
+        "not-found",
+        "投稿の取得に失敗しました",
+        "algolia"
+      );
+    });
+
+  return { posts, hit };
+};
+
+const fetchFirestore = async (data, posts) => {
+  for (let i = 0; i < posts.length; i++) {
+    posts[i] &&
+      (await db
+        .collection(data.index)
+        .doc(posts[i].uid)
+        .get()
+        .then((doc) => {
+          if (doc.exists) {
+            posts[i].icon = doc.data().icon;
+          }
+        })
+        .catch((e) => {
+          throw new functions.https.HttpsError(
+            "not-found",
+            "ユーザーの取得に失敗しました",
+            "firebase"
+          );
+        }));
+  }
+};
