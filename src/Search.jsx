87f99d072fb304f53fe169f0e0b497fb377e3aa5
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2021-10-22 23:29:32 +0900

    不具合 修正

--- src/Search.jsx
+++ src/Search.jsx
@@ -33,7 +33,7 @@ export const Search = () => {
     !search.control &&
       dispatch(
         fetchPosts({
-          index: "matters",
+          index: index,
           value: search.value,
           target: search.target,
           type: search.type,
