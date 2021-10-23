fe93587d3000271b948ebdac7c4ecfb78937277a
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2021-10-23 09:32:45 +0900

    try catch 削除

--- src/features/user/functions/auth/handleReset.js
+++ src/features/user/functions/auth/handleReset.js
@@ -2,8 +2,9 @@ import { auth } from "../../../../firebase";
 import * as rootSlice from "../../../root/rootSlice";
 
 export const handleReset = async ({ dispatch, reset, setReset, data }) => {
-  try {
-    await auth.sendPasswordResetEmail(data.reset).then(() => {
+  await auth
+    .sendPasswordResetEmail(data.reset)
+    .then(() => {
       setReset(!reset);
       dispatch(
         rootSlice.handleAnnounce({
@@ -11,13 +12,13 @@ export const handleReset = async ({ dispatch, reset, setReset, data }) => {
           text: "登録しているメールアドレスに再送信しました",
         })
       );
+    })
+    .catch((e) => {
+      dispatch(
+        rootSlice.handleAnnounce({
+          type: "error",
+          text: "メールアドレスが存在しません",
+        })
+      );
     });
-  } catch (e) {
-    dispatch(
-      rootSlice.handleAnnounce({
-        type: "error",
-        text: "メールアドレスが存在しません",
-      })
-    );
-  }
 };
