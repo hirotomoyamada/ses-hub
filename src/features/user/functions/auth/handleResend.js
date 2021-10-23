bca01f8a78643a1729ead5233f4f853f43894540
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2021-10-23 09:32:45 +0900

    try catch 削除

--- src/features/user/functions/auth/handleResend.js
+++ src/features/user/functions/auth/handleResend.js
@@ -2,33 +2,24 @@ import { auth } from "../../../../firebase";
 import * as rootSlice from "../../../root/rootSlice";
 
 export const handleResend = async ({ dispatch }) => {
-  try {
-    await auth.currentUser
-      .sendEmailVerification({
-        url: "https://ses-hub.app/login",
-      })
-      .then(() => {
-        dispatch(
-          rootSlice.handleAnnounce({
-            type: "success",
-            text: "登録しているメールアドレスに再送信しました",
-          })
-        );
-      })
-      .catch((e) => {
-        dispatch(
-          rootSlice.handleAnnounce({
-            type: "error",
-            text: "再度時間をおいてください",
-          })
-        );
-      });
-  } catch (e) {
-    dispatch(
-      rootSlice.handleAnnounce({
-        type: "error",
-        text: "再送信に失敗しました",
-      })
-    );
-  }
+  await auth.currentUser
+    .sendEmailVerification({
+      url: "https://ses-hub.app/login",
+    })
+    .then(() => {
+      dispatch(
+        rootSlice.handleAnnounce({
+          type: "success",
+          text: "登録しているメールアドレスに再送信しました",
+        })
+      );
+    })
+    .catch((e) => {
+      dispatch(
+        rootSlice.handleAnnounce({
+          type: "error",
+          text: "再度時間をおいてください",
+        })
+      );
+    });
 };
--- src/features/user/functions/auth/handleSignUp.js
+++ src/features/user/functions/auth/handleSignUp.js
@@ -9,35 +9,40 @@ export const handleSignUp = async ({
   data,
 }) => {
   const { email, password } = data;
-  try {
-    await auth
-      .createUserWithEmailAndPassword(email, password)
-      .then(async (e) => {
-        await auth.currentUser
-          .sendEmailVerification({
-            url: "https://ses-hub.app/login",
-          })
-          .then(() => {
-            setCreate(true);
-            setEmail(true);
-          })
-          .catch((e) => {
-            dispatch(
-              rootSlice.handleAnnounce({
-                type: "error",
-                text: "再度時間をおいてください",
-              })
-            );
-          });
+
+  await auth
+    .createUserWithEmailAndPassword(email, password)
+    .then(async (e) => {
+      await auth.currentUser
+        .sendEmailVerification({
+          url: "https://ses-hub.app/login",
+        })
+        .then(() => {
+          setCreate(true);
+          setEmail(true);
+          methods.reset();
+        })
+        .catch((e) => {
+          console.log(e);
+          dispatch(
+            rootSlice.handleAnnounce({
+              type: "error",
+              text: "再度時間をおいてください",
+            })
+          );
+        });
+    })
+    .catch((e) => {
+      dispatch(
+        rootSlice.handleAnnounce({
+          type: "error",
+          text: "アカウントの作成に失敗しました",
+        })
+      );
+      methods.reset({
+        email: data.email,
+        password: "",
+        verifiedPassword: "",
       });
-    methods.reset();
-  } catch (e) {
-    dispatch(
-      rootSlice.handleAnnounce({
-        type: "error",
-        text: "アカウントの作成に失敗しました",
-      })
-    );
-    methods.reset({ email: data.email, password: "", verifiedPassword: "" });
-  }
+    });
 };
