0f7ee5fd47b2cae8f22f5e050a13bf49ac9b9f6c
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2021-10-28 00:16:17 +0900

    hook 実装

--- src/features/post/list/List.jsx
+++ src/features/post/list/List.jsx
@@ -1,14 +1,9 @@
 import styles from "./List.module.scss";
 
-import { useEffect, useState, useRef } from "react";
-import { useDispatch } from "react-redux";
-
 import { Posts } from "./components/Posts";
 import { NotFound } from "./components/NotFound";
 import { Load } from "./components/Load";
-
-import { createObserver } from "./functions/createObserver";
-import { fetchScroll } from "./functions/fetchScroll";
+import { useFetch } from "./hook/useFetch";
 
 export const List = ({
   index,
@@ -28,61 +23,18 @@ export const List = ({
   bests,
   open,
 }) => {
-  const dispatch = useDispatch();
-
-  const load = useRef();
-  const list = useRef();
-
-  const [page, setPage] = useState(0);
-  const [intersecting, setIntersecting] = useState(false);
-
-  useEffect(() => {
-    !bests && setPage(hit?.currentPage);
-    !bests && setIntersecting(false);
-  }, [bests, hit?.currentPage, hit?.pages]);
-
-  useEffect(() => {
-    const observer = createObserver(
-      list,
-      bests,
-      hit,
-      page,
-      setPage,
-      intersecting,
-      setIntersecting
-    );
-
-    const ref = load.current;
-    ref && observer?.observe(ref);
-
-    return () => {
-      ref && observer?.unobserve(ref);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [hit?.pages, intersecting, page]);
-
-  useEffect(() => {
-    !bests &&
-      intersecting &&
-      hit.pages &&
-      page !== hit.pages &&
-      fetchScroll(
-        dispatch,
-        index,
-        user,
-        home,
-        search,
-        companys,
-        sort,
-        type,
-        select,
-        page
-      ).then(() => {
-        setIntersecting(!intersecting);
-      });
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [page]);
+  const [list, load, page] = useFetch(
+    index,
+    hit,
+    user,
+    home,
+    search,
+    companys,
+    sort,
+    type,
+    select,
+    bests
+  );
 
   return (
     <div className={select && styles.list_scroll}>
