08f28a49533931a6246319cfc4a8b603b33585cc
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2021-10-28 14:13:22 +0900

    state 追加

--- src/features/user/layouts/main/components/profile/components/person/components/Head.jsx
+++ src/features/user/layouts/main/components/profile/components/person/components/Head.jsx
@@ -3,12 +3,31 @@ import styles from "../Person.module.scss";
 
 export const Head = ({ user, currentUser }) => {
   return (
-    <div className={styles.profile_wrap}>
-      <h1 className={styles.profile_name}>{user?.profile?.nickName}</h1>
+    <div className={styles.profile_col}>
+      <div className={styles.profile_wrap}>
+        <h1 className={styles.profile_name}>{user?.profile?.nickName}</h1>
 
-      <div className={styles.profile_position}>{user?.profile?.position}</div>
+        <Menu index="persons" post={user} user={currentUser} person />
+      </div>
 
-      <Menu index="persons" post={user} user={currentUser} person />
+      <div className={styles.profile_wrap}>
+        <div className={styles.profile_category}>{user?.profile?.position}</div>
+
+        <div
+          className={`${styles.profile_category} ${
+            styles.profile_category_acnt
+          } ${
+            (user?.profile?.state === "確定" ||
+              user?.profile?.state === "商談中" ||
+              user?.profile?.state === "情報収集中") &&
+            styles.profile_category_disable
+          } ${
+            user?.profile?.state === "至急" && styles.profile_category_hurry
+          }`}
+        >
+          {user?.profile?.state}
+        </div>
+      </div>
     </div>
   );
 };
