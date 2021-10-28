f09fa5bfb05f6e8728fb6fc2f6188d68613ab443
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2021-10-28 14:13:22 +0900

    state 追加

--- src/features/post/item/components/user/header/Header.jsx
+++ src/features/post/item/components/user/header/Header.jsx
@@ -9,10 +9,28 @@ export const Header = ({ post }) => {
         <Icon src={post?.icon} />
       </div>
 
-      <div className={styles.header_wrap}>
-        <h1 className={styles.header_ttl}>
-          {post?.profile?.person ? post.profile.person : post.profile.nickName}
-        </h1>
+      <div className={styles.header_container}>
+        <div className={styles.header_wrap}>
+          <h1 className={styles.header_ttl}>
+            {post?.profile?.person
+              ? post.profile.person
+              : post.profile.nickName}
+          </h1>
+          {post?.profile?.state && (
+            <span
+              className={`${styles.header_state} ${
+                (post?.profile?.state === "確定" ||
+                  post?.profile?.state === "商談中" ||
+                  post?.profile?.state === "情報収集中") &&
+                styles.header_state_disable
+              } ${
+                post?.profile?.state === "至急" && styles.header_state_hurry
+              }`}
+            >
+              {post?.profile?.state}
+            </span>
+          )}
+        </div>
         <h2 className={styles.header_tag}>
           {post?.profile?.name ? post.profile.name : post.profile.position}
         </h2>
