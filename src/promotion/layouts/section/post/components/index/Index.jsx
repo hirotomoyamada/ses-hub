153e66f5fdf1a3e04c8673b609be7d94619fd47d
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2021-10-30 15:19:20 +0900

    ディレクトリ 変更

--- src/promotion/layouts/section/post/components/index/Index.jsx
+++ src/promotion/layouts/section/post/components/index/Index.jsx
@@ -1,17 +1,17 @@
-import styles from "../Post.module.scss";
+import styles from "./Index.module.scss";
 
 import { useDispatch } from "react-redux";
-import * as rootSlice from "../../../../../features/root/rootSlice";
+import * as rootSlice from "../../../../../../features/root/rootSlice";
 
 export const Index = ({ index }) => {
   const dispatch = useDispatch();
 
   return (
-    <div className={styles.post_index}>
+    <div className={styles.index}>
       <button
         type="button"
-        className={`${styles.post_index_btn} ${
-          index === "matters" && styles.post_index_btn_current
+        className={`${styles.index_btn} ${
+          index === "matters" && styles.index_btn_current
         }`}
         onClick={() => dispatch(rootSlice.handleIndex("matters"))}
       >
@@ -19,8 +19,8 @@ export const Index = ({ index }) => {
       </button>
       <button
         type="button"
-        className={`${styles.post_index_btn} ${
-          index === "resources" && styles.post_index_btn_current
+        className={`${styles.index_btn} ${
+          index === "resources" && styles.index_btn_current
         }`}
         onClick={() => dispatch(rootSlice.handleIndex("resources"))}
       >
