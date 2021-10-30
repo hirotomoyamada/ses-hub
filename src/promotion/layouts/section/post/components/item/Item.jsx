c3f667c31eee0d7f3ff12fd094eafb39e977c063
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2021-10-30 15:16:00 +0900

    ディレクトリ 変更

--- src/promotion/layouts/section/post/components/item/Item.jsx
+++ src/promotion/layouts/section/post/components/item/Item.jsx
@@ -1,6 +1,6 @@
 import styles from "./Item.module.scss";
-import { Matters } from "./Matters";
-import { Resources } from "./Resources";
+import { Matters } from "./components/Matters";
+import { Resources } from "./components/Resources";
 
 export const Item = ({ index, post }) => {
   return (
