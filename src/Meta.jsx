1bad49cf2ca9cf099e844673095784a26b059bd8
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2021-11-03 13:15:38 +0900

    カラー 変更

--- src/Meta.jsx
+++ src/Meta.jsx
@@ -4,6 +4,10 @@ export const Meta = () => {
   return (
     <Helmet>
       <title>{`SES_HUB｜営業支援App`}</title>
+      <link
+        rel="icon"
+        href={`${process.env.PUBLIC_URL}/img/logo/favicon.svg`}
+      />
     </Helmet>
   );
 };
