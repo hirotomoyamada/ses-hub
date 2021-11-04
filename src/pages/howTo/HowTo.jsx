ba18203097aa748d05c2e84369c726d4c3d4e447
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2021-11-04 14:20:27 +0900

    スマートフォン アクセス時トップへ位置取り

--- src/pages/howTo/HowTo.jsx
+++ src/pages/howTo/HowTo.jsx
@@ -1,6 +1,6 @@
 import styles from "./HowTo.module.scss";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { Header } from "../../components/header/Header";
 import { Menu } from "./components/menu/Menu";
@@ -9,10 +9,14 @@ import { Main } from "./components/Main/Main";
 export const HowTo = () => {
   const [page, setPage] = useState("home");
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className={styles.howto}>
       <Header back />
-      
+
       <div className={styles.howto_inner}>
         <Menu page={page} setPage={setPage} />
         <Main page={page} />
