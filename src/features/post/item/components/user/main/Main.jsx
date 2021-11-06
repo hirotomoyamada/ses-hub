8248af43641fc9b49d4c1805a4af14e914203df7
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2021-11-06 13:31:26 +0900

    不具合 修正

--- src/features/post/item/components/user/main/Main.jsx
+++ src/features/post/item/components/user/main/Main.jsx
@@ -8,10 +8,12 @@ import { Body } from "./components/Body";
 
 export const Main = ({ index, post }) => {
   return index !== "persons" ? (
-    post?.profile?.body && (
+    post?.profile?.body ? (
       <div className={styles.main_companys}>
         <p className={styles.main_companys_body}>{post?.profile?.body}</p>
       </div>
+    ) : (
+      <></>
     )
   ) : (
     <div className={styles.main}>
