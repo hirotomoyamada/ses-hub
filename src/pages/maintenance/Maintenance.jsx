bce29fd02ba6423140e2523eff6186b4d8782fc6
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2021-11-07 17:05:24 +0900

    メンテナンス 修正

--- src/pages/maintenance/Maintenance.jsx
+++ src/pages/maintenance/Maintenance.jsx
@@ -4,8 +4,7 @@ import { useSelector } from "react-redux";
 import * as rootSlice from "../../features/root/rootSlice";
 
 export const Maintenance = () => {
-  const maintenance = "disable";
-  // useSelector(rootSlice.data).maintenance?.status;
+  const maintenance = useSelector(rootSlice.data).maintenance?.status;
 
   return (
     maintenance === "enable" && (
