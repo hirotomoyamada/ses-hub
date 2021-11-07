b6c824df6f89fd7a5cc71d502663a9a177d1e637
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2021-11-07 17:28:26 +0900

    アクセス時 トップに戻す

--- src/pages/asct/Asct.jsx
+++ src/pages/asct/Asct.jsx
@@ -1,8 +1,15 @@
 import styles from "./Asct.module.scss";
-import { Table } from "./components/Table";
+
+import { useEffect } from "react";
+
 import { Header } from "../../components/header/Header";
+import { Table } from "./components/Table";
 
 export const Asct = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div className={styles.asct}>
       <Header ttl="特定商取引法に基づく表示" back />
