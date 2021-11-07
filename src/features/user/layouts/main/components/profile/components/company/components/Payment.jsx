1a579107f7feaac29f3bdf3a7dab9d03ac30aa62
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2021-11-07 10:44:48 +0900

    オプション 表示

--- src/features/user/layouts/main/components/profile/components/company/components/Payment.jsx
+++ src/features/user/layouts/main/components/profile/components/company/components/Payment.jsx
@@ -2,19 +2,29 @@ import styles from "../Company.module.scss";
 
 export const Payment = ({ user }) => {
   return (
-    <div
-      className={`${styles.profile_payment} ${
-        user?.payment?.status === "active"
-          ? styles.profile_payment_active
-          : user?.payment?.status === "trialing" &&
-            styles.profile_payment_trialing
-      }`}
-    >
-      {user?.payment?.status === "active"
-        ? "レギュラー"
-        : user?.payment?.status === "trialing"
-        ? "レギュラー(フリートライアル)"
-        : "リミテッド"}
+    <div className={styles.profile_wrap}>
+      <div
+        className={`${styles.profile_payment} ${
+          user?.payment?.status === "active"
+            ? styles.profile_payment_active
+            : user?.payment?.status === "trialing" &&
+              styles.profile_payment_trialing
+        }`}
+      >
+        {user?.payment?.status === "active"
+          ? "レギュラー"
+          : user?.payment?.status === "trialing"
+          ? "レギュラー(フリートライアル)"
+          : "リミテッド"}
+      </div>
+
+      {user?.payment?.option?.freelanceDirect && (
+        <div
+          className={`${styles.profile_payment} ${styles.profile_payment_option}`}
+        >
+          フリーランスダイレクト
+        </div>
+      )}
     </div>
   );
 };
