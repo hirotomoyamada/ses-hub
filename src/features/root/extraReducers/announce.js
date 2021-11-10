043fc9eeef731273aab001660e394f9049ccbc44
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2021-11-10 12:41:29 +0900

    アカウント情報 ソーシャル認証 アナウンス表示

--- src/features/root/extraReducers/announce.js
+++ src/features/root/extraReducers/announce.js
@@ -1,12 +1,5 @@
 export const announce = (builder) => {
   builder.addMatcher(
-    (action) => action.type.endsWith("/addProvider"),
-    (state) => {
-      state.announce.success = "認証されました";
-    }
-  );
-
-  builder.addMatcher(
     (action) =>
       action.type.endsWith("/createPost/fulfilled") ||
       action.type.endsWith("/fetchPosts/fulfilled") ||
