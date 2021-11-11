a032d75ff352da9e72a02c9b0c71046b6304a8a2
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2021-11-11 17:49:20 +0900

    追加

--- src/features/user/functions/account/functions.js
+++ src/features/user/functions/account/functions.js
@@ -1,4 +1,13 @@
 import { handleAuth } from "./handleAuth";
 import { handleReset } from "./handleReset";
+import { handleCreate } from "./handleCreate";
+import { handleDelete } from "./handleDelete";
+import { handleVerification } from "./handleVerification";
 
-export { handleAuth, handleReset };
+export {
+  handleAuth,
+  handleReset,
+  handleCreate,
+  handleDelete,
+  handleVerification,
+};
