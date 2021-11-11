d5c650da3c4343e2b87996602bdc704fbbfc101b
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2021-11-11 17:48:39 +0900

    不具合 修正

--- src/pages/account/components/main/components/account/Account.jsx
+++ src/pages/account/components/main/components/account/Account.jsx
@@ -1,7 +1,49 @@
-import { Btn } from "../btn/Btn";
 import styles from "./Account.module.scss";
 
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router";
+
+import * as rootSlice from "../../../../../../features/root/rootSlice";
+
+import { Btn } from "../btn/Btn";
+
+import * as functions from "../../../../../../features/user/functions/functions";
+
 export const Account = ({ user, current }) => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const handlePlan = () => {
+    history.push("/plan");
+  };
+
+  const handleCreate = () => {
+    dispatch(
+      rootSlice.handleModal({
+        type: "account",
+        meta: { type: "create", target: user?.profile?.email },
+      })
+    );
+  };
+
+  const handleDelete = () => {
+    dispatch(
+      rootSlice.handleModal({
+        type: "account",
+        meta: { type: "delete", target: user?.profile?.email },
+      })
+    );
+  };
+
+  const handleReset = () => {
+    const email = user?.profile?.email;
+
+    functions.account.handleReset({
+      dispatch,
+      email,
+    });
+  };
+
   return (
     <div className={`${styles.account} ${current && styles.account_current}`}>
       {current && (
@@ -13,15 +55,21 @@ export const Account = ({ user, current }) => {
           {user?.profile?.email ? user?.profile?.email : "作成されていません"}
         </span>
 
-        {current && <Btn plan />}
+        {current && <Btn type="plan" txt="プランの変更" func={handlePlan} />}
 
         {!current && (
           <div className={styles.account_btn}>
-            {user?.profile?.email && <Btn create />}
+            {!user?.profile?.email && (
+              <Btn type="create" txt="作成" func={handleCreate} />
+            )}
 
-            {!user?.profile?.email && <Btn reset />}
+            {user?.profile?.email && (
+              <Btn type="reset" txt="パスワード再設定" func={handleReset} />
+            )}
 
-            {!user?.profile?.email && <Btn remove />}
+            {user?.profile?.email && (
+              <Btn type="delete" txt="削除" func={handleDelete} />
+            )}
           </div>
         )}
       </div>
