fb2ddca0838cd12995bdd10b5818679e95a3e953
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2021-11-11 11:35:06 +0900

    payment 情報を元に表示

--- src/pages/setting/page/main/components/Account.jsx
+++ src/pages/setting/page/main/components/Account.jsx
@@ -1,23 +1,29 @@
 import styles from "../Main.module.scss";
 
 export const Account = ({ user, history }) => {
-  return (
+  return user.type === "corporate" ? (
     <div className={styles.main_row}>
       <div className={styles.main_col}>
         <span className={styles.main_tag}>アカウント</span>
         <span className={styles.main_value}>
-          4&nbsp;アカウントまで作成できます
+          {user?.payment?.account ? user?.payment?.account - 1 : 0}
+          &nbsp;アカウントまで作成できます
         </span>
         {user?.provider && user.provider.indexOf("password") < 0 && (
           <span className={styles.main_desc}>
             メールログインを有効にする必要があります
           </span>
         )}
+        {user?.payment?.status === "canceled" && (
+          <span className={styles.main_desc}>
+            プランを選択する必要があります
+          </span>
+        )}
       </div>
       <button
         className={`${styles.main_btn} ${
-          user?.provider &&
-          user.provider.indexOf("password") < 0 &&
+          ((user?.provider && user.provider.indexOf("password") < 0) ||
+            user?.payment?.status === "canceled") &&
           styles.main_btn_disabled
         }`}
         type="button"
@@ -26,5 +32,7 @@ export const Account = ({ user, history }) => {
         管理
       </button>
     </div>
+  ) : (
+    <></>
   );
 };
