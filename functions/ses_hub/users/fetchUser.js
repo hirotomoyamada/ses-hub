467b79014c46a8f853f4e5137aca1f2313037967
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2021-11-11 12:41:29 +0900

    account用ロジック追加

--- functions/ses_hub/users/fetchUser.js
+++ functions/ses_hub/users/fetchUser.js
@@ -28,69 +28,119 @@ exports.fetchUser = functions
   });
 
 const fetchProfile = async (context, data, demo) => {
-  const index = algolia.initIndex(data.index);
+  const user = await fetchAlgolia(data, demo);
 
-  const user = await index
-    .getObject(data.uid)
-    .then((hit) => {
-      return hit && data.index === "companys"
-        ? fetch.companys({ hit: hit, demo: demo })
-        : data.index === "persons" && fetch.persons({ hit: hit, demo: demo });
-    })
-    .catch((e) => {
-      throw new functions.https.HttpsError(
-        "not-found",
-        "プロフィールの取得に失敗しました",
-        "algolia"
-      );
-    });
-
-  await db
-    .collection(data.index)
-    .doc(data.uid)
-    .get()
-    .then((doc) => {
-      if (doc.exists) {
-        user.icon = doc.data().icon;
-        user.cover = doc.data().cover;
-
-        if (data.index === "companys") {
-          user.type = doc.data().type;
-        }
-
-        if (data.index === "persons") {
-          const request =
-            doc.data().requests?.enable?.indexOf(context.auth.uid) >= 0
-              ? "enable"
-              : doc.data().requests?.hold?.indexOf(context.auth.uid) >= 0
-              ? "hold"
-              : doc.data().requests?.disable?.indexOf(context.auth.uid) >= 0
-              ? "hold"
-              : "none";
-
-          if (request !== "enable") {
-            user.profile.name = dummy("person");
-            user.profile.email = dummy("email");
-            user.profile.urls = dummy("urls", 3);
-
-            user.resume = null;
-          } else {
-            user.resume = doc.data().resume.url;
-          }
-
-          user.request = request;
-        }
-      }
-    })
-    .catch((e) => {
-      throw new functions.https.HttpsError(
-        "not-found",
-        "ユーザーの取得に失敗しました",
-        "firebase"
-      );
-    });
+  await fetchFirestore(context, data, user);
 
   return user;
+};
+
+const fetchAlgolia = async (data, demo) => {
+  const index = algolia.initIndex(data.index);
+
+  const user = !data.uids
+    ? await index
+        .getObject(data.uid)
+        .then((hit) => {
+          return hit && data.index === "companys"
+            ? fetch.companys({ hit: hit, demo: demo })
+            : data.index === "persons" &&
+                fetch.persons({ hit: hit, demo: demo });
+        })
+        .catch((e) => {
+          throw new functions.https.HttpsError(
+            "not-found",
+            "プロフィールの取得に失敗しました",
+            "algolia"
+          );
+        })
+    : await index
+        .getObjects(data.uids)
+        .then(({ results }) => {
+          return results.map(
+            (hit) => hit && fetch.companys({ hit: hit, demo: demo })
+          );
+        })
+        .catch((e) => {
+          throw new functions.https.HttpsError(
+            "not-found",
+            "プロフィールの取得に失敗しました",
+            "algolia"
+          );
+        });
+
+  return user;
+};
+
+const fetchFirestore = async (context, data, user) => {
+  if (!data.uids) {
+    await db
+      .collection(data.index)
+      .doc(data.uid)
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          user.icon = doc.data().icon;
+          user.cover = doc.data().cover;
+
+          if (data.index === "companys") {
+            user.type = doc.data().type;
+          }
+
+          if (data.index === "persons") {
+            const request =
+              doc.data().requests?.enable?.indexOf(context.auth.uid) >= 0
+                ? "enable"
+                : doc.data().requests?.hold?.indexOf(context.auth.uid) >= 0
+                ? "hold"
+                : doc.data().requests?.disable?.indexOf(context.auth.uid) >= 0
+                ? "hold"
+                : "none";
+
+            if (request !== "enable") {
+              user.profile.name = dummy("person");
+              user.profile.email = dummy("email");
+              user.profile.urls = dummy("urls", 3);
+
+              user.resume = null;
+            } else {
+              user.resume = doc.data().resume.url;
+            }
+
+            user.request = request;
+          }
+        }
+      })
+      .catch((e) => {
+        throw new functions.https.HttpsError(
+          "not-found",
+          "ユーザーの取得に失敗しました",
+          "firebase"
+        );
+      });
+  } else {
+    for (let i = 0; i < user.length; i++) {
+      user[i] &&
+        (await db
+          .collection(data.index)
+          .doc(user[i].uid)
+          .get()
+          .then((doc) => {
+            if (doc.exists) {
+              user[i].icon = doc.data().icon;
+              user[i].cover = doc.data().cover;
+              user[i].type = doc.data().type;
+            }
+          })
+          .catch((e) => {
+            throw new functions.https.HttpsError(
+              "not-found",
+              "ユーザーの取得に失敗しました",
+              "firebase"
+            );
+          }));
+    }
+  }
 };
 
 const fetchBests = async (user, data) => {
