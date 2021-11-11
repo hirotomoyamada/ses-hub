7df5dae25fcefad36de509d950bed9332db197a9
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2021-11-11 17:49:20 +0900

    追加

---
+++ src/features/user/functions/account/handleDelete.js
@@ -0,0 +1,6 @@
+// import firebase from "firebase/app";
+// import { auth } from "../../../../firebase";
+
+// import * as rootSlice from "../../../root/rootSlice";
+
+export const handleDelete = async () => {};
