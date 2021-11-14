fc461055195d4f8e3af16d5a67007da15865940f
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2021-11-14 13:03:25 +0900

    グループアカウント 名称変更

--- src/pages/account/components/main/components/header/Header.jsx
+++ src/pages/account/components/main/components/header/Header.jsx
@@ -3,7 +3,7 @@ import styles from "./Header.module.scss";
 export const Header = () => {
   return (
     <div className={styles.header}>
-      <h1 className={styles.header_ttl}>アカウント</h1>
+      <h1 className={styles.header_ttl}>グループアカウント</h1>
       <p>現在、保有しているアカウントの一覧です</p>
     </div>
   );
