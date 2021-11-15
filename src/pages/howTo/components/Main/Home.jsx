61bd66e44fbbda5a20dfc64828d9e9f711028be9
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2021-11-15 17:22:47 +0900

    文章 修正

--- src/pages/howTo/components/Main/Home.jsx
+++ src/pages/howTo/components/Main/Home.jsx
@@ -11,13 +11,25 @@ export const Home = () => {
       <div className={styles.howto_container}>
         <Tag tag="自分の投稿" free paid />
         <p>
-          ホームでは、案件や人材の表示を切り替えることができ、自分の投稿が案件・人材別に表示されます。
+          <span className={styles.howto_container_tag}>ホーム</span>
+          では、<span className={styles.howto_container_tag}>案件</span>
+          <span className={styles.howto_container_tag}>人材</span>
+          の表示を切り替えることができ、自分の投稿が
+          <span className={styles.howto_container_tag}>案件</span>
+          <span className={styles.howto_container_tag}>人材</span>
+          別に表示されます。
         </p>
       </div>
       <div className={styles.howto_container}>
         <Tag tag="フォローしているメンバーの投稿" paid />
         <p>
-          レギュラー会員では、自分の投稿とフォローしているメンバーの投稿もタイムライン上に表示されます。
+          <span className={styles.howto_container_tag}>レギュラー会員</span>
+          では、
+          <span className={styles.howto_container_tag}>自分の投稿</span>や
+          <span className={styles.howto_container_tag}>
+            フォローしているメンバーの投稿
+          </span>
+          もタイムライン上に表示されます。
           <br />
           <br />
           <span className={styles.howto_container_acnt}>
@@ -25,7 +37,8 @@ export const Home = () => {
           </span>
           <br />
           <span className={styles.howto_container_acnt}>
-            ※ フォローを15名以上している場合、ホームの設定から表示したいメンバーをいつでも設定・変更できます。
+            ※
+            フォローを15名以上している場合、ホームの設定から表示したいメンバーをいつでも設定・変更できます。
           </span>
         </p>
       </div>
