4019588ca7000028fa5b658cfe29ad72f31619a7
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2021-11-15 17:02:05 +0900

    plan/account/requests を追加

--- src/pages/howTo/components/Main/Posts.jsx
+++ src/pages/howTo/components/Main/Posts.jsx
@@ -5,35 +5,68 @@ export const Posts = () => {
   return (
     <div className={styles.howto_main}>
       <h1 className={styles.howto_ttl}>投稿</h1>
-      <p className={styles.howto_desc}>案件・人材別に投稿することが可能です</p>
+      <p className={styles.howto_desc}>案件・人材別に投稿する</p>
       <div className={styles.howto_container}>
         <Tag tag="投稿する" free paid />
         <p>
-          SES_HUB独自のフォーマットですべてのメンバーが統一された情報を入力、管理することが可能です。人による記入の違いがなくなり、見やすい投稿ができます。
+          SES_HUB独自のフォーマットですべてのメンバーが統一された情報を入力、管理することが可能です。
+          <br />
+          <br />
+          人による記入の違いがなくなり、見やすい投稿ができます。
         </p>
       </div>
       <div className={styles.howto_container}>
         <Tag tag="公開する(デフォルト)" free paid />
         <p>
-          デフォルトで投稿作成時に「公開」が選択されています。公開された投稿は、リミテッド会員・レギュラー会員ともにすべてのメンバーに公開されます。
+          デフォルトで投稿作成時に
+          <span className={styles.howto_container_tag}>公開</span>
+          が選択されています。公開された投稿は、
+          <span className={styles.howto_container_tag}>リミテッド会員</span>
+          <span className={styles.howto_container_tag}>レギュラー会員</span>
+          ともにすべてのメンバーに公開されます。
           <span className={styles.howto_container_acnt}>
-            また、レギュラー会員はリミテッド会員の公開された投稿を見ることができますので、リミテッド会員にたいして問い合わせることが可能です。
+            <br />
+            <br />
+            ※ レギュラー会員はリミテッド会員の公開された投稿を見ることができますので、リミテッド会員にたいして問い合わせることが可能です。
           </span>
         </p>
       </div>
       <div className={styles.howto_container}>
         <Tag tag="非公開する" free paid />
         <p>
-          非公開にした投稿は、ホームや検索に表示されず、自分のメンバーページやいいね・出力リストだけ確認することが可能です。投稿後も、いつでも公開・非公開の設定が可能です。
-          ※検索やホームには反映されません。また、全体に反映されるまで5分ほど処理がかかる場合があります。
+          非公開にした投稿は、ホームや検索
+          <span className={styles.howto_container_tag}>ホーム</span>
+          <span className={styles.howto_container_tag}>検索</span>
+          に表示されず、自分の
+          <span className={styles.howto_container_tag}>メンバーページ</span>
+          <span className={styles.howto_container_tag}>いいね</span>
+          <span className={styles.howto_container_tag}>出力</span>
+          リストだけ確認することが可能です。
+          <br />
+          <br />
+          投稿後も、いつでも
+          <span className={styles.howto_container_tag}>公開</span>
+          <span className={styles.howto_container_tag}>非公開</span>
+          の設定が可能です。
+          <br />
+          <br />※ 全体に反映されるまで5分ほど処理がかかる場合があります。
         </p>
       </div>
       <div className={styles.howto_container}>
         <Tag tag="ステータスで管理する" free paid />
         <p>
-          一つ一つの投稿へステータスを付与できます。ステータスには、「新規」「提案」「面談」「保留」「フォロー」「成約」などを設定できます。設定したステータスは自分のメンバーページで確認できます。
+          一つ一つの投稿へステータスを付与できます。ステータスには、
+          <span className={styles.howto_container_tag}>新規</span>
+          <span className={styles.howto_container_tag}>提案</span>
+          <span className={styles.howto_container_tag}>面談</span>
+          <span className={styles.howto_container_tag}>保留</span>
+          <span className={styles.howto_container_tag}>フォロー</span>
+          <span className={styles.howto_container_tag}>成約</span>
+          などを設定できます。設定したステータスは自分のメンバーページで確認できます。
+          <br />
+          <br />
           <span className={styles.howto_container_acnt}>
-            また、他のメンバーにステータスは表示されません。
+            ※ 他のメンバーにステータスは表示されません。
           </span>
         </p>
       </div>
--- src/pages/howTo/components/tag/Tag.jsx
+++ src/pages/howTo/components/tag/Tag.jsx
@@ -1,6 +1,6 @@
 import styles from "./Tag.module.scss";
 
-export const Tag = ({ tag, free, paid }) => {
+export const Tag = ({ tag, free, paid, option }) => {
   return (
     <div className={styles.tag}>
       <span className={styles.tag_name}>{tag}</span>
@@ -12,6 +12,11 @@ export const Tag = ({ tag, free, paid }) => {
       {paid && (
         <span className={`${styles.tag_type} ${styles.tag_type_paid}`}>
           レギュラー
+        </span>
+      )}
+      {option && (
+        <span className={`${styles.tag_type} ${styles.tag_type_option}`}>
+          オプション
         </span>
       )}
     </div>
