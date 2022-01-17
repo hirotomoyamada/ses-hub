70ff9ff8bf22cad4019b5bfbbe1e6e06d3d616f9
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-01-17 16:49:50 +0900

    子アカウント メールアドレス変更 追加

--- src/features/user/reducers/redurces.js
+++ src/features/user/reducers/redurces.js
@@ -3,7 +3,7 @@ import { fetchUser } from "./fetchUser";
 import { resetUser } from "./resetUser";
 
 import { updateToken } from "./token";
-import { createChild, deleteChild } from "./child";
+import { createChild, changeEmailChild, deleteChild } from "./child";
 
 import { editProfile } from "./editProfile";
 import { enableAgree } from "./enableAgree";
@@ -26,6 +26,7 @@ export {
   resetUser,
   updateToken,
   createChild,
+  changeEmailChild,
   deleteChild,
   editProfile,
   enableAgree,
