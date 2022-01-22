f148e482c8063ab9873c8042da159126fee4ce0a
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2022-01-22 11:07:11 +0900

    コード整形

--- functions/admin/posts/deletePost.js
+++ functions/admin/posts/deletePost.js
@@ -16,43 +16,53 @@ exports.deletePost = functions
       );
     }
 
-    const index = algolia.initIndex(data.index);
-    await index
-      .deleteObject(data.post.objectID)
-      .then(async () => {
-        await db
-          .collection("companys")
-          .doc(data.post.uid)
-          .get()
-          .then((doc) => {
-            if (doc.exists) {
-              const posts = doc
-                .data()
-                .posts[data.index].filter(
-                  (objectID) => objectID !== data.post.objectID
-                );
+    await deleteFirestore(data);
+    await deleteAlgolia(data);
 
-              doc.ref.set(
-                {
-                  posts: { [data.index]: [...posts] },
-                },
-                { merge: true }
-              );
-            }
-          })
-          .catch((e) => {
-            throw new functions.https.HttpsError(
-              "data-loss",
-              "投稿の削除に失敗しました",
-              "firebase"
-            );
-          });
-      })
-      .catch((e) => {
-        throw new functions.https.HttpsError(
-          "data-loss",
-          "投稿の削除に失敗しました",
-          "algolia"
-        );
-      });
+    return;
   });
+
+const deleteFirestore = async (data) => {
+  await db
+    .collection("companys")
+    .doc(data.post.uid)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        const posts = doc
+          .data()
+          .posts[data.index].filter(
+            (objectID) => objectID !== data.post.objectID
+          );
+
+        doc.ref.set(
+          {
+            posts: { [data.index]: [...posts] },
+          },
+          { merge: true }
+        );
+      }
+    })
+    .catch((e) => {
+      throw new functions.https.HttpsError(
+        "data-loss",
+        "投稿の削除に失敗しました",
+        "firebase"
+      );
+    });
+
+  return;
+};
+
+const deleteAlgolia = async (data) => {
+  const index = algolia.initIndex(data.index);
+  await index.deleteObject(data.post.objectID).catch((e) => {
+    throw new functions.https.HttpsError(
+      "data-loss",
+      "投稿の削除に失敗しました",
+      "algolia"
+    );
+  });
+
+  return;
+};
--- functions/admin/root/sendMail.js
+++ functions/admin/root/sendMail.js
@@ -17,118 +17,99 @@ exports.sendMail = functions
       );
     }
 
-    await db
-      .collection(
-        data.index === "companys"
-          ? "seshub"
-          : data.index === "persons" && "freelanceDirect"
-      )
-      .doc("mail")
-      .get()
-      .then(async (doc) => {
-        data.updateAt = Date.now();
+    const mail = await createMail(data);
 
-        if (doc.exists) {
-          await doc.ref.set(data, { merge: true }).catch((e) => {
-            throw new functions.https.HttpsError(
-              "data-loss",
-              "データの更新に失敗しました",
-              "firebase"
-            );
-          });
-        }
-      });
-
-    const verified = (email) => {
-      const config = functions.config();
-
-      return (
-        config.admin.ses_hub !== email &&
-        config.admin.freelance_direct !== email &&
-        config.demo.ses_hub.email !== email &&
-        config.demo.freelance_direct.email !== email &&
-        true
-      );
-    };
-
-    const to =
-      data.index === "companys"
-        ? await db
-            .collection("companys")
-            .where("status", "==", "enable")
-            .where(
-              "payment.status",
-              data.target !== "all" ? "==" : "in",
-              data.target !== "all"
-                ? data.target
-                : ["active", "canceled", "trialing"]
-            )
-            .get()
-            .then((querySnapshot) => {
-              // 本番コード
-              return querySnapshot.docs.map(
-                (doc) => verified(doc) && doc.data().profile.email
-              );
-
-              // テストコード
-              // const emails = querySnapshot.docs.map((doc) => {
-              //   const email = doc.data().profile.email;
-              //   const name = email.substring(0, email.indexOf("@"));
-              //   const dummy = name.concat("@sink.sendgrid.net");
-
-              //   return verified(email) && dummy;
-              // });
-
-              // return emails.filter((email) => email);
-            })
-        : await db
-            .collection("persons")
-            .where("status", "==", "enable")
-            .get()
-            .then((querySnapshot) => {
-              // 本番コード
-              return querySnapshot.docs.map(
-                (doc) => verified(doc) && doc.data().profile.email
-              );
-
-              // テストコード
-              // const emails = querySnapshot.docs.map((doc) => {
-              //   const email = doc.data().profile.email;
-              //   const name = email.substring(0, email.indexOf("@"));
-              //   const dummy = name.concat("@sink.sendgrid.net");
-
-              //   return verified(email) && dummy;
-              // });
-
-              // return emails.filter((email) => email);
-            });
-
-    // テストコード
-    // let to = [];
-
-    // for (let i = 0; i < 100; i++) {
-    //   const name = ["yamada", "ito", "sato", "kato", "suzuki"][
-    //     Math.floor(Math.random() * 5)
-    //   ];
-    //   const sub = Math.random().toString(32).substring(2);
-    //   const domain = "@sink.sendgrid.net";
-    //   const email = name.concat(sub, domain);
-
-    //   to = [email, ...to];
-    // }
-
-    const mail = {
-      to: to,
-      from:
-        data.index === "companys"
-          ? `SES_HUB <${functions.config().admin.ses_hub}>`
-          : data.index === "persons" &&
-            `Freelance Direct <${functions.config().admin.freelance_direct}>`,
-      subject: data.title,
-      text: data.body,
-    };
-
+    await updateFirestore(data);
     await send(mail);
 
     return data;
   });
+
+const createMail = async (data) => {
+  const to = await fetchTo(data);
+  const from =
+    data.index === "companys"
+      ? `SES_HUB <${functions.config().admin.ses_hub}>`
+      : data.index === "persons" &&
+        `Freelance Direct <${functions.config().admin.freelance_direct}>`;
+
+  const title = data.title;
+  const body = data.body;
+
+  return {
+    to: to,
+    from: from,
+    subject: title,
+    text: body,
+  };
+};
+
+const fetchTo = async (data) => {
+  const to =
+    data.index === "companys"
+      ? await db
+          .collection("companys")
+          .where("status", "==", "enable")
+          .where(
+            "payment.status",
+            data.target !== "all" ? "==" : "in",
+            data.target !== "all"
+              ? data.target
+              : ["active", "canceled", "trialing"]
+          )
+          .get()
+          .then((querySnapshot) => {
+            return querySnapshot.docs.map(
+              (doc) => verified(doc) && doc.data().profile.email
+            );
+          })
+      : await db
+          .collection("persons")
+          .where("status", "==", "enable")
+          .get()
+          .then((querySnapshot) => {
+            return querySnapshot.docs.map(
+              (doc) => verified(doc) && doc.data().profile.email
+            );
+          });
+
+  return to;
+};
+
+const updateFirestore = async (data) => {
+  await db
+    .collection(
+      data.index === "companys"
+        ? "seshub"
+        : data.index === "persons" && "freelanceDirect"
+    )
+    .doc("mail")
+    .get()
+    .then(async (doc) => {
+      data.updateAt = Date.now();
+
+      if (doc.exists) {
+        await doc.ref.set(data, { merge: true }).catch((e) => {
+          throw new functions.https.HttpsError(
+            "data-loss",
+            "データの更新に失敗しました",
+            "firebase"
+          );
+        });
+      }
+    });
+
+  return;
+};
+
+const verified = (email) => {
+  const config = functions.config();
+
+  return (
+    config.admin.ses_hub !== email &&
+    config.admin.freelance_direct !== email &&
+    config.demo.ses_hub.email !== email &&
+    config.demo.freelance_direct.email !== email &&
+    true
+  );
+};
