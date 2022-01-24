66cfc02c254ec236e8241176e505eb250be86e3e
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2022-01-24 13:10:00 +0900

    コード 整形

--- functions/ses_hub/posts/sendPost.js
+++ functions/ses_hub/posts/sendPost.js
@@ -19,14 +19,10 @@ exports.sendPost = functions
       canceled: true,
     });
 
-<<<<<<< HEAD
-    if (data.post.display === "private") {
-=======
     const index = data.index;
     const post = data.post;
 
     if (post.display === "private") {
->>>>>>> main
       throw new functions.https.HttpsError(
         "cancelled",
         "非公開の投稿のため、処理中止",
@@ -34,72 +30,16 @@ exports.sendPost = functions
       );
     }
 
-<<<<<<< HEAD
-    const {
-      mail, // SendGrid
-      // text // Twitter
-    } = await createMail(data.index, data.post);
-
-    // Twitter 投稿
-    // await twitter.tweet(text);
-
-    // SendGrid メール
-=======
     const user = await fetchUser(post);
     const to = await fetchTo(post);
     const mail = createMail(index, post, user, to);
 
     // await tweet(mail.text);
-
->>>>>>> main
     await send(mail);
 
     return;
   });
 
-<<<<<<< HEAD
-const fetchUser = async (post) => {
-  const user = await db
-    .collection("companys")
-    .doc(post.uid)
-    .get()
-    .then((doc) => {
-      return {
-        name: doc.data().profile.name,
-        person: doc.data().profile.person,
-      };
-    });
-
-  return user;
-};
-
-const fetchTo = async (post) => {
-  const verified = (doc) => {
-    const id = doc.id;
-    const email = doc.data().profile.email;
-    const config = functions.config();
-
-    return (
-      post.uid !== id &&
-      config.admin.ses_hub !== email &&
-      config.demo.ses_hub.email !== email &&
-      true
-    );
-  };
-
-  const to = await db
-    .collection("companys")
-    .where("status", "==", "enable")
-    .get()
-    .then((querySnapshot) => {
-      // 本番コード
-      const emails = querySnapshot.docs.map(
-        (doc) => verified(doc) && doc.data().profile.email
-      );
-
-      return emails.filter((email) => email);
-    })
-=======
 const createMail = (index, post, user, to) => {
   const url = `${functions.config().app.ses_hub.url}/${index}/${post.objectID}`;
 
@@ -139,7 +79,6 @@ const fetchTo = async (post) => {
     .collection("companys")
     .where("status", "==", "enable")
     .get()
->>>>>>> main
     .catch((e) => {
       throw new functions.https.HttpsError(
         "not-found",
@@ -148,39 +87,6 @@ const fetchTo = async (post) => {
       );
     });
 
-<<<<<<< HEAD
-  return to;
-};
-
-const createMail = async (index, post) => {
-  const to = await fetchTo(post);
-  const user = await fetchUser(post);
-
-  const subject =
-    index === "matters"
-      ? `【新着案件】 ${post.title}`
-      : index === "resources" &&
-        `【新着人材】 ${post.roman.firstName.substring(
-          0,
-          1
-        )} . ${post.roman.lastName.substring(0, 1)}`;
-
-  const url = `${functions.config().app.ses_hub.url}/${index}/${post.objectID}`;
-
-  const text =
-    index === "matters"
-      ? body.matters(post, user, url)
-      : index === "resources" && body.resources(post, user, url);
-
-  const mail = {
-    to: to,
-    from: `SES_HUB <${functions.config().admin.ses_hub}>`,
-    subject: subject,
-    text: text,
-  };
-
-  return { mail, text };
-=======
   return querySnapshot?.docs
     ?.map((doc) => verified(doc, post) && doc.data().profile.email)
     ?.filter((email) => email);
@@ -197,5 +103,4 @@ const verified = (doc, post) => {
     config.demo.ses_hub.email !== email &&
     true
   );
->>>>>>> main
 };
