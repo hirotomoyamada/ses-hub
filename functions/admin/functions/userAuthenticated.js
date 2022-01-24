f6b6d6c5922255efaf483b4668ba023c97c659b1
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2022-01-24 18:47:31 +0900

    不具合 修正

--- functions/admin/functions/userAuthenticated.js
+++ functions/admin/functions/userAuthenticated.js
@@ -1,6 +1,6 @@
 const functions = require("firebase-functions");
 
-exports.userAuthenticated = async ({ context }) => {
+exports.userAuthenticated = async (context) => {
   if (context.auth.uid !== functions.config().admin.uid) {
     throw new functions.https.HttpsError(
       "cancelled",
