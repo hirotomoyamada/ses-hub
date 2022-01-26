45c24bf36e910d2ee5ae5f95f7f8590f73277598
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-01-26 17:49:45 +0900

    不具合 修正

--- src/pages/setting/page/main/components/Account.jsx
+++ src/pages/setting/page/main/components/Account.jsx
@@ -6,16 +6,24 @@ export const Account = ({ user, history }) => {
       <div className={styles.main_col}>
         <span className={styles.main_tag}>グループアカウント</span>
         <span className={styles.main_value}>
-          {user?.payment?.status !== "canceled" && user?.payment?.account
+          {user?.payment?.status !== "canceled" &&
+          user?.payment?.account &&
+          user?.payment?.children?.length
             ? user?.payment?.account - user?.payment?.children?.length - 1
+            : user?.payment?.status !== "canceled" &&
+              user?.payment?.account &&
+              !user?.payment?.children?.length
+            ? user?.payment?.account - 1
             : 0}
           &nbsp;アカウントまで作成できます
         </span>
+
         {user?.provider && user.provider.indexOf("password") < 0 && (
           <span className={styles.main_desc}>
             メールログインを有効にする必要があります
           </span>
         )}
+
         {user?.payment?.status === "canceled" && (
           <span className={styles.main_desc}>
             プランを選択する必要があります
