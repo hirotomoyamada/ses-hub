5e52c7c7298223675bcae161a20d7d83b552da74
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-02-05 15:27:19 +0900

    不具合 修正

--- src/features/pay/components/freetrial/FreeTrial.jsx
+++ src/features/pay/components/freetrial/FreeTrial.jsx
@@ -1,7 +1,7 @@
 import styles from "./FreeTrial.module.scss";
 
 export const FreeTrial = ({ user }) => {
-  return user?.payment?.trial && user?.type === "individaul" ? (
+  return user?.payment?.trial && user?.type === "individual" ? (
     <div className={styles.free}>
       <p className={styles.free_desc}>\ キャンペーン中 /</p>
       <h1 className={styles.free_ttl}>フリートライアル</h1>
--- src/features/post/Meta.jsx
+++ src/features/post/Meta.jsx
@@ -4,7 +4,7 @@ export const Meta = ({ post }) => {
   return (
     <Helmet>
       <title>
-        {post.title ? `${post.title}｜SES_HUB` : "SES_HUB｜営業支援App"}
+        {post?.title ? `${post.title}｜SES_HUB` : "SES_HUB｜営業支援App"}
       </title>
     </Helmet>
   );
