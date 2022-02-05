49c7534775d97669bbeacb83a4a8742fce473a5a
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2022-02-05 15:35:24 +0900

    閲覧・問い合わせ 追加 / 他項目 校正

--- src/promotion/layouts/section/feature/Feature.jsx
+++ src/promotion/layouts/section/feature/Feature.jsx
@@ -30,7 +30,7 @@ export const Feature = () => {
 
           <tbody>
             <tr>
-              <td>案件・人材登録</td>
+              <td>案件・人材を登録</td>
 
               <td>
                 <RadioButtonUncheckedIcon
@@ -38,6 +38,52 @@ export const Feature = () => {
                 />
               </td>
 
+              <td>
+                <RadioButtonUncheckedIcon
+                  className={`${styles.feature_icon} ${styles.feature_icon_enable}`}
+                />
+              </td>
+            </tr>
+
+            <tr>
+              <td>情報を検索</td>
+
+              <td>
+                <RadioButtonUncheckedIcon
+                  className={`${styles.feature_icon} ${styles.feature_icon_enable}`}
+                />
+              </td>
+              <td>
+                <RadioButtonUncheckedIcon
+                  className={`${styles.feature_icon} ${styles.feature_icon_enable}`}
+                />
+              </td>
+            </tr>
+
+            <tr>
+              <td>案件・人材を閲覧</td>
+
+              <td>
+                <RadioButtonUncheckedIcon
+                  className={`${styles.feature_icon} ${styles.feature_icon_enable}`}
+                />
+                <span className={styles.feature_icon_desc}>※1</span>
+              </td>
+              <td>
+                <RadioButtonUncheckedIcon
+                  className={`${styles.feature_icon} ${styles.feature_icon_enable}`}
+                />
+              </td>
+            </tr>
+
+            <tr>
+              <td>案件・人材へ問い合わせ</td>
+
+              <td>
+                <RadioButtonUncheckedIcon
+                  className={`${styles.feature_icon} ${styles.feature_icon_enable}`}
+                />
+              </td>
               <td>
                 <RadioButtonUncheckedIcon
                   className={`${styles.feature_icon} ${styles.feature_icon_enable}`}
@@ -52,24 +98,9 @@ export const Feature = () => {
                 <ChangeHistoryIcon
                   className={`${styles.feature_icon} ${styles.feature_icon_limited}`}
                 />
-                <span className={styles.feature_icon_desc}>※1</span>
+                <span className={styles.feature_icon_desc}>※2</span>
               </td>
 
-              <td>
-                <RadioButtonUncheckedIcon
-                  className={`${styles.feature_icon} ${styles.feature_icon_enable}`}
-                />
-              </td>
-            </tr>
-
-            <tr>
-              <td>情報検索</td>
-
-              <td>
-                <CloseIcon
-                  className={`${styles.feature_icon} ${styles.feature_icon_disable}`}
-                />
-              </td>
               <td>
                 <RadioButtonUncheckedIcon
                   className={`${styles.feature_icon} ${styles.feature_icon_enable}`}
@@ -84,7 +115,7 @@ export const Feature = () => {
                 <ChangeHistoryIcon
                   className={`${styles.feature_icon} ${styles.feature_icon_limited}`}
                 />
-                <span className={styles.feature_icon_desc}>※2</span>
+                <span className={styles.feature_icon_desc}>※3</span>
               </td>
               <td>
                 <RadioButtonUncheckedIcon
@@ -120,7 +151,7 @@ export const Feature = () => {
                 <RadioButtonUncheckedIcon
                   className={`${styles.feature_icon} ${styles.feature_icon_enable}`}
                 />
-                <span className={styles.feature_icon_desc}>※3</span>
+                <span className={styles.feature_icon_desc}>※4</span>
               </td>
             </tr>
 
@@ -151,30 +182,37 @@ export const Feature = () => {
                 <RadioButtonUncheckedIcon
                   className={`${styles.feature_icon} ${styles.feature_icon_enable}`}
                 />
-                <span className={styles.feature_icon_desc}>※4</span>
+                <span className={styles.feature_icon_desc}>※5</span>
               </td>
             </tr>
           </tbody>
         </table>
 
         <span className={styles.feature_announce}>
-          ※1 無料プランは、新着投稿の通知を受け取ることが出来ても、ご自身の投稿は他のユーザーに通知されることはありません。
+          ※1&nbsp;無料プランは、月&nbsp;10&nbsp;回まで閲覧可能です。
         </span>
         <br />
         <span className={styles.feature_announce}>
-          ※2 ご自身の案件・人材情報は外部へ出力することが可能です。
+          ※2&nbsp;無料プランは、新着投稿の通知を受け取ることが出来ても、ご自身の投稿は他のユーザーに通知されることはありません。
         </span>
         <br />
         <span className={styles.feature_announce}>
-          ※3 別途オプションの加入が必要です。
+          ※3&nbsp;ご自身の案件・人材情報は外部へ出力することが可能です。
         </span>
         <br />
         <span className={styles.feature_announce}>
-          ※4 複数のアカウントを登録・管理するには法人契約が必要です。
+          ※4&nbsp;別途オプションの加入が必要です。
+          <span className={styles.feature_announce_desc}>
+            今なら無料体験中 !
+          </span>
         </span>
         <br />
         <span className={styles.feature_announce}>
-          ※ 今後のアップデートにより機能は変更になる場合がございます。
+          ※5&nbsp;複数のアカウントを登録・管理するには法人契約が必要です。
+        </span>
+        <br />
+        <span className={styles.feature_announce}>
+          ※&nbsp;今後のアップデートにより機能は変更になる場合がございます。
         </span>
       </div>
     </section>
