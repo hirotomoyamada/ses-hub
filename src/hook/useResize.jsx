93d3d3d62f485c60f599bb6debaaa43231225447
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2022-02-09 03:36:16 +0900

    ディレクトリ 移動

---
+++ src/hook/useResize.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState, useRef } from "react";
+
+export const useResize = (verified) => {
+  const inner = useRef();
+
+  const [resize, setResize] = useState(false);
+
+  useEffect(() => {
+    const resizeObserver = () => {
+      const innerHeight = inner?.current?.getBoundingClientRect().height;
+      const windowHeight = window.innerHeight;
+
+      innerHeight > windowHeight ? setResize(true) : setResize(false);
+    };
+
+    const observer = new ResizeObserver(() => {
+      resizeObserver();
+    });
+
+    const ref = inner?.current;
+
+    window.addEventListener("resize", () => {
+      resizeObserver();
+    });
+    ref && observer?.observe(ref);
+
+    return () => {
+      window.removeEventListener("resize", resizeObserver);
+      ref && observer.disconnect(ref);
+    };
+  }, [inner, resize, verified]);
+
+  return [resize, inner];
+};
