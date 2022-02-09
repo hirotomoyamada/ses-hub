b066767a9d510c33a0aa888cfe4cb5e894d0031e
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2022-02-09 23:09:58 +0900

    統合

--- src/pages/user/components/main/components/profile/components/company/components/CreateAt.jsx
+++ src/pages/user/components/main/components/profile/components/company/components/CreateAt.jsx
@@ -3,19 +3,14 @@ import styles from "../Company.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCalendarAlt } from "@fortawesome/free-regular-svg-icons";
 
-export const CreateAt = ({ user }) => {
-  const timestamp = (t) => {
-    const unix = new Date(t);
-    const year = unix.getFullYear();
-    const month = ("0" + (unix.getMonth() + 1)).slice(-2);
-    return `${year}年${month}月`;
-  };
+import * as functions from "../../../../../../../../../functions/functions";
 
+export const CreateAt = ({ user }) => {
   return (
     <div className={styles.profile_createAt}>
       <FontAwesomeIcon icon={faCalendarAlt} className={styles.profile_icon} />
       <span>
-        {timestamp(user?.createAt)}
+        {functions.root.timestamp(user?.createAt)}
         からSES_HUBを利用しています
       </span>
     </div>
