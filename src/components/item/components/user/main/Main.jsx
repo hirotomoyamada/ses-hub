bc0dcfd5c085e8fc74fe4031e386379be9775572
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2022-02-12 15:59:21 +0900

    プロフィールの自己紹介 デフォルト文 追加

--- src/components/item/components/user/main/Main.jsx
+++ src/components/item/components/user/main/Main.jsx
@@ -8,13 +8,13 @@ import { Body } from "./components/Body";
 
 export const Main = ({ index, post }) => {
   return index !== "persons" ? (
-    post?.profile?.body ? (
-      <div className={styles.main_companys}>
-        <p className={styles.main_companys_body}>{post?.profile?.body}</p>
-      </div>
-    ) : (
-      <></>
-    )
+    <div className={styles.main_companys}>
+      <p className={styles.main_companys_body}>
+        {post?.profile?.body
+          ? post?.profile?.body
+          : "さぁ、あなたのプロフィールを充実させていきましょう。SNSアカウントを登録して他のメンバーと情報連携していきましょう!"}
+      </p>
+    </div>
   ) : (
     <div className={styles.main}>
       <div className={styles.main_side}>
