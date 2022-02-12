1efb635c31921752a9a4ff1a84d29f60aa8e8dc5
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-02-12 15:59:21 +0900

    プロフィールの自己紹介 デフォルト文 追加

--- src/pages/user/components/main/components/profile/components/company/components/Body.jsx
+++ src/pages/user/components/main/components/profile/components/company/components/Body.jsx
@@ -4,6 +4,8 @@ export const Body = ({ user }) => {
   return user?.profile?.body ? (
     <p className={styles.profile_body}>{user?.profile?.body}</p>
   ) : (
-    <></>
+    <p className={styles.profile_body}>
+      さぁ、あなたのプロフィールを充実させていきましょう。SNSアカウントを登録して他のメンバーと情報連携していきましょう!
+    </p>
   );
 };
