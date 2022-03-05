08c30b24d62630936d088969e8876e0da0158d2e
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2022-03-06 05:53:49 +0900

    プロフィール未入力ユーザーのbodyの表示が正しくない 不具合 修正

--- src/pages/user/components/main/components/profile/components/company/components/Body.tsx
+++ src/pages/user/components/main/components/profile/components/company/components/Body.tsx
@@ -11,7 +11,7 @@ export const Body: React.FC<PropType> = ({ user }) => {
   return user?.profile?.body ? (
     <p className={styles.profile_body}>{user?.profile?.body}</p>
   ) : (
-    <p className={styles.profile_body}>
+    <p className={`${styles.profile_body} ${styles.profile_body_none}`}>
       さぁ、あなたのプロフィールを充実させていきましょう。SNSアカウントを登録して他のメンバーと情報連携していきましょう!
     </p>
   );
