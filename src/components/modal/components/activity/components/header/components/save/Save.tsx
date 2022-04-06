2ec86c50798c657997df0cc90596402ee72c18d0
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-04-06 23:06:47 +0900

    submitされない不具合 修正

--- src/components/modal/components/activity/components/header/components/save/Save.tsx
+++ src/components/modal/components/activity/components/header/components/save/Save.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./Save.module.scss";
 export const Save: React.FC = () => {
   return (
-    <button type="submit" className={styles.save}>
+    <button type="submit" className={styles.save} form="setting">
       保存
     </button>
   );
