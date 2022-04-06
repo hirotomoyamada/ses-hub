958f29f2b1af2b00d3f562d4dd03ecdc1ac4f08e
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2022-04-06 12:36:58 +0900

    setting レイアウト 追加

--- src/components/modal/components/activity/components/header/Header.tsx
+++ src/components/modal/components/activity/components/header/Header.tsx
@@ -5,13 +5,16 @@ import { Span } from "./components/span/Span";
 import { Sort } from "./components/sort/Sort";
 
 import * as Activity from "components/modal/components/activity/Activity";
+import { Save } from "./components/save/Save";
 
 interface PropType {
   type?: "user" | "post";
   span: Activity.Span;
   sort: Activity.Sort;
+  setting: boolean;
   setSpan: React.Dispatch<React.SetStateAction<Activity.Span>>;
   setSort: React.Dispatch<React.SetStateAction<Activity.Sort>>;
+  setSetting: React.Dispatch<React.SetStateAction<boolean>>;
   handleClose: () => void;
 }
 
@@ -19,26 +22,33 @@ export const Header: React.FC<PropType> = ({
   type,
   span,
   sort,
+  setting,
   setSpan,
   setSort,
+  setSetting,
   handleClose,
 }) => {
   return (
     <div
       className={`${styles.header} ${type === "user" && styles.header_user}`}
     >
-      <button onClick={handleClose} className={styles.header_cancel}>
-        もどる
+      <button
+        onClick={!setting ? handleClose : () => setSetting(false)}
+        className={styles.header_cancel}
+      >
+        {!setting ? "とじる" : "もどる"}
       </button>
 
       <p className={styles.header_ttl}>アクティビティ</p>
 
-      {type === "user" && (
+      {type === "user" && !setting && (
         <div className={styles.header_command}>
           <Sort sort={sort} setSort={setSort} />
           <Span span={span} setSpan={setSpan} />
         </div>
       )}
+
+      {setting && <Save />}
     </div>
   );
 };
