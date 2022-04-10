653d7f93ce96196370c458f9ad1d98eedc1e55da
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-04-10 15:47:37 +0900

    fetchActivity 追加

--- src/components/modal/components/activity/components/post/Post.tsx
+++ src/components/modal/components/activity/components/post/Post.tsx
@@ -1,17 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "../../Activity.module.scss";
 
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 import { Command } from "./components/command/Command";
 import { Detail } from "./components/detail/Detail";
-import { View } from "./components/view/View";
+import { History } from "./components/history/History";
 import { Today } from "./components/today/Today";
 import { Log } from "./components/log/Log";
 
 import * as postSlice from "features/post/postSlice";
 
 import { Matter, Resource } from "types/post";
+import { fetchActivity } from "features/post/actions";
 
 interface PropType {
   index: "matters" | "resources" | "companys" | "persons";
@@ -19,13 +20,23 @@ interface PropType {
 }
 
 export const Post: React.FC<PropType> = ({ index, post }) => {
+  const dispatch = useDispatch();
   const activity = useSelector(postSlice.activity);
+
+  useEffect(() => {
+    if (post.objectID && (index === "matters" || index === "resources"))
+      dispatch(fetchActivity({ index: index, objectId: post.objectID }));
+
+    return () => {
+      dispatch(postSlice.resetPost());
+    };
+  }, [dispatch, index, post]);
 
   return (
     <div className={styles.activity_inner}>
       <Detail index={index} post={post} />
       <Command total={activity.total} />
-      <View total={activity.total} />
+      <History total={activity.total} />
       <Today today={activity.today} />
       <Log log={activity.log} />
     </div>
