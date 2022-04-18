875f573a45d11adbf9a9efbb3a0ba4d497fa06f5
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-04-18 11:54:18 +0900

    フォローされています 表示

--- src/pages/user/components/main/components/profile/components/company/components/Head.tsx
+++ src/pages/user/components/main/components/profile/components/company/components/Head.tsx
@@ -10,7 +10,13 @@ interface PropType {
 export const Head: React.FC<PropType> = ({ user }) => {
   return (
     <div>
-      <h1 className={styles.profile_person}>{user?.profile?.person}</h1>
+      <div className={styles.profile_wrap}>
+        <h1 className={styles.profile_person}>{user?.profile?.person}</h1>
+
+        {"followed" in user && user.followed && (
+          <span className={styles.profile_followed}>フォローされています</span>
+        )}
+      </div>
 
       <h2 className={styles.profile_name}>{user?.profile?.name}</h2>
     </div>
