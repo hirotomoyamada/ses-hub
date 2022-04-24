4ffe9dc96e826447e2244ff5fbc6ef322cc7d2a0
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2022-04-24 16:30:00 +0900

    add fetchActivity

--- src/components/modal/components/activity/components/user/User.tsx
+++ src/components/modal/components/activity/components/user/User.tsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../../Activity.module.scss";
 
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
+import * as rootSlice from "features/root/rootSlice";
+import * as userSlice from "features/user/userSlice";
+
+import { fetchActivity } from "features/user/actions";
+
+import { Audio } from "react-loader-spinner";
 import { Charts } from "./components/charts/Charts";
 import { Csv } from "./components/csv/Csv";
 
 import { Span, Sort } from "components/modal/components/activity/Activity";
 import { Btn } from "./components/btn/Btn";
-import * as userSlice from "features/user/userSlice";
 
 import { User as UserType } from "types/user";
 import { Setting } from "./components/setting/Setting";
@@ -19,7 +24,6 @@ interface PropType {
   sort: Sort;
   setting: boolean;
   setSetting: React.Dispatch<React.SetStateAction<boolean>>;
-  setVerification: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export const User: React.FC<PropType> = ({
@@ -28,19 +32,31 @@ export const User: React.FC<PropType> = ({
   sort,
   setting,
   setSetting,
-  setVerification,
 }) => {
+  const dispatch = useDispatch();
   const activity = useSelector(userSlice.activity);
+  const fetch = useSelector(rootSlice.load).fetch;
+  const [uid, setUid] = useState<string>(user.uid);
+  const active = useSelector(rootSlice.setting)?.activity?.active;
+  const order = useSelector(rootSlice.setting)?.activity?.order;
 
-  return !setting ? (
+  useEffect(() => {
+    dispatch(fetchActivity({ uid, span, active, order }));
+  }, [dispatch, uid, span]);
+
+  return !setting && !fetch ? (
     <div className={`${styles.activity_inner} ${styles.activity_inner_user}`}>
       <Charts span={span} sort={sort} activity={activity} />
       <Csv span={span} sort={sort} />
       <Btn setting={setting} setSetting={setSetting} />
     </div>
+  ) : fetch ? (
+    <div className={`${styles.activity_inner} ${styles.activity_fetch}`}>
+      <Audio color="#49b757" height={48} width={48} />
+    </div>
   ) : (
     <div className={styles.activity_inner}>
-      <Setting activity={activity} setVerification={setVerification} />
+      <Setting activity={activity} />
       <Btn setting={setting} setSetting={setSetting} />
     </div>
   );
