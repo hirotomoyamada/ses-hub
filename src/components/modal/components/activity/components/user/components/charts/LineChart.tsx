53c4abab254f554ef7278ec8dd4651b459d94536
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-04-24 18:02:07 +0900

    React.memo化

--- src/components/modal/components/activity/components/user/components/charts/LineChart.tsx
+++ src/components/modal/components/activity/components/user/components/charts/LineChart.tsx
@@ -16,63 +16,58 @@ interface PropType {
   sort?: Sort;
 }
 
-export const LineChart: React.FC<PropType> = ({
-  width,
-  height,
-  sample,
-  setting,
-  data,
-  sort,
-}) => {
-  return (
-    <LC
-      width={width}
-      height={height}
-      data={data ? [...data.log].reverse() : undefined}
-    >
-      <XAxis dataKey="label" hide />
+export const LineChart: React.FC<PropType> = React.memo(
+  ({ width, height, sample, setting, data, sort }) => {
+    return (
+      <LC
+        width={width}
+        height={height}
+        data={data ? [...data.log].reverse() : undefined}
+      >
+        <XAxis dataKey="label" hide />
 
-      <Tooltip
-        separator=""
-        contentStyle={{
-          borderRadius: "12px",
-          paddingTop: "16px",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "flex-start",
-          gap: "8px",
-        }}
-        itemStyle={{
-          display: "flex",
-          gap: "2px",
-        }}
-        labelStyle={{
-          fontWeight: "bold",
-        }}
-        formatter={(value: number, name: string) => [
-          value,
-          name === "self" ? "した数" : "された数",
-        ]}
-      />
-
-      {(sample || sort?.self || data?.key === "posts") && (
-        <Line
-          type="linear"
-          dataKey="self"
-          stroke={setting?.activity?.color?.self || "#49b657"}
-          animationEasing="ease-in-out"
+        <Tooltip
+          separator=""
+          contentStyle={{
+            borderRadius: "12px",
+            paddingTop: "16px",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "flex-start",
+            gap: "8px",
+          }}
+          itemStyle={{
+            display: "flex",
+            gap: "2px",
+          }}
+          labelStyle={{
+            fontWeight: "bold",
+          }}
+          formatter={(value: number, name: string) => [
+            value,
+            name === "self" ? "した数" : "された数",
+          ]}
         />
-      )}
 
-      {(sample || (sort?.others && !isNaN(Number(data?.others)))) && (
-        <Line
-          type="linear"
-          dataKey="others"
-          stroke={setting?.activity?.color?.others || "#ff9900"}
-          animationEasing="ease-in-out"
-        />
-      )}
-    </LC>
-  );
-};
+        {(sample || sort?.self || data?.key === "posts") && (
+          <Line
+            type="linear"
+            dataKey="self"
+            stroke={setting?.activity?.color?.self || "#49b657"}
+            animationEasing="ease-in-out"
+          />
+        )}
+
+        {(sample || (sort?.others && !isNaN(Number(data?.others)))) && (
+          <Line
+            type="linear"
+            dataKey="others"
+            stroke={setting?.activity?.color?.others || "#ff9900"}
+            animationEasing="ease-in-out"
+          />
+        )}
+      </LC>
+    );
+  }
+);
--- src/components/modal/components/activity/components/user/components/charts/NumberChart.tsx
+++ src/components/modal/components/activity/components/user/components/charts/NumberChart.tsx
@@ -14,53 +14,50 @@ interface PropType {
   sort?: Sort;
   data?: Activity[number];
 }
-export const NumberChart: React.FC<PropType> = ({
-  sample,
-  setting,
-  sort,
-  data,
-}) => {
-  return (
-    <div className={styles.number}>
-      {data?.log.map((log, i) => (
-        <div key={i} className={styles.number_container}>
-          <p className={styles.number_label}>{log.label}</p>
+export const NumberChart: React.FC<PropType> = React.memo(
+  ({ sample, setting, sort, data }) => {
+    return (
+      <div className={styles.number}>
+        {data?.log.map((log, i) => (
+          <div key={i} className={styles.number_container}>
+            <p className={styles.number_label}>{log.label}</p>
 
-          <div className={styles.number_wrap}>
-            {(sample ||
-              (sort?.self &&
-                log?.self !== null &&
-                !isNaN(Number(log?.self)))) && (
-              <CountUp
-                className={`${styles.number_total}`}
-                style={{ color: setting?.activity?.color?.self || "#49b657" }}
-                start={0}
-                end={log?.self || 0}
-                separator=","
-                duration={1.5}
-                useEasing={true}
-              />
-            )}
+            <div className={styles.number_wrap}>
+              {(sample ||
+                (sort?.self &&
+                  log?.self !== null &&
+                  !isNaN(Number(log?.self)))) && (
+                <CountUp
+                  className={`${styles.number_total}`}
+                  style={{ color: setting?.activity?.color?.self || "#49b657" }}
+                  start={0}
+                  end={log?.self || 0}
+                  separator=","
+                  duration={1.5}
+                  useEasing={true}
+                />
+              )}
 
-            {(sample ||
-              (sort?.others &&
-                log?.others !== null &&
-                !isNaN(Number(log?.others)))) && (
-              <CountUp
-                className={`${styles.number_total}`}
-                style={{
-                  color: setting?.activity?.color?.others || "#ff9900",
-                }}
-                start={0}
-                end={log?.others || 0}
-                separator=","
-                duration={1.5}
-                useEasing={true}
-              />
-            )}
+              {(sample ||
+                (sort?.others &&
+                  log?.others !== null &&
+                  !isNaN(Number(log?.others)))) && (
+                <CountUp
+                  className={`${styles.number_total}`}
+                  style={{
+                    color: setting?.activity?.color?.others || "#ff9900",
+                  }}
+                  start={0}
+                  end={log?.others || 0}
+                  separator=","
+                  duration={1.5}
+                  useEasing={true}
+                />
+              )}
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
-  );
-};
+        ))}
+      </div>
+    );
+  }
+);
