c6ab85d8c5184dc81c87f528c70600c32a7bf55a
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-04-29 12:23:58 +0900

    Activity User 子アカウントの情報を取得

--- src/components/modal/components/activity/components/user/User.tsx
+++ src/components/modal/components/activity/components/user/User.tsx
@@ -6,9 +6,8 @@ import { useDispatch, useSelector } from "react-redux";
 import * as rootSlice from "features/root/rootSlice";
 import * as userSlice from "features/user/userSlice";
 
-import { fetchActivity } from "features/user/actions";
+import { fetchActivity, fetchUser } from "features/user/actions";
 
-import { Audio } from "react-loader-spinner";
 import { Charts } from "./components/charts/Charts";
 import { Csv } from "./components/csv/Csv";
 
@@ -17,6 +16,8 @@ import { Btn } from "./components/btn/Btn";
 
 import { User as UserType } from "types/user";
 import { Setting } from "./components/setting/Setting";
+import { Account } from "./components/account/Account";
+import { Company } from "types/post";
 
 interface PropType {
   user: UserType;
@@ -34,25 +35,37 @@ export const User: React.FC<PropType> = ({
   setSetting,
 }) => {
   const dispatch = useDispatch();
+  const selectUser = useSelector(userSlice.selectUser) as Company[];
   const [uid, setUid] = useState<string>(user.uid);
   const activity = useSelector(userSlice.activity)?.[uid];
-  const fetch = useSelector(rootSlice.load).fetch;
   const active = useSelector(rootSlice.setting)?.activity?.active;
   const order = useSelector(rootSlice.setting)?.activity?.order;
+  const status = user?.payment?.status !== "canceled";
+  const children = user?.payment?.children;
 
   useEffect(() => {
     dispatch(fetchActivity({ uid, span, active, order }));
   }, [dispatch, uid, span]);
 
-  return !setting && !fetch ? (
+  useEffect(() => {
+    status &&
+      activity?.length &&
+      !selectUser?.length &&
+      children?.length &&
+      dispatch(
+        fetchUser({
+          index: "companys",
+          uids: children,
+        })
+      );
+  }, [dispatch, user, status, children, activity]);
+
+  return !setting ? (
     <div className={`${styles.activity_inner} ${styles.activity_inner_user}`}>
+      <Account user={user} selectUser={selectUser} uid={uid} setUid={setUid} />
       <Charts span={span} sort={sort} activity={activity} />
-      <Csv span={span} sort={sort} />
-      <Btn setting={setting} setSetting={setSetting} />
-    </div>
-  ) : fetch ? (
-    <div className={`${styles.activity_inner} ${styles.activity_fetch}`}>
-      <Audio color="#49b757" height={48} width={48} />
+      <Csv span={span} sort={sort} activity={activity} />
+      <Btn setting={setting} setSetting={setSetting} activity={activity} />
     </div>
   ) : (
     <div className={styles.activity_inner}>
