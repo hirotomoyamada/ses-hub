253a72995fe78e38e81d08760dc02895a6ec3b91
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2022-04-29 12:24:19 +0900

    子アカウントを取得することで、アニメーションを再調整

--- src/components/modal/components/activity/components/user/components/btn/Btn.tsx
+++ src/components/modal/components/activity/components/user/components/btn/Btn.tsx
@@ -3,14 +3,20 @@ import styles from "./Btn.module.scss";
 
 import SettingsIcon from "@material-ui/icons/Settings";
 import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
+import { Activity } from "features/user/initialState";
+import { useSelector } from "react-redux";
+import * as rootSlice from "features/root/rootSlice";
 
 interface PropType {
   setting: boolean;
   setSetting: React.Dispatch<React.SetStateAction<boolean>>;
+  activity?: Activity;
 }
 
-export const Btn: React.FC<PropType> = ({ setting, setSetting }) => {
-  return (
+export const Btn: React.FC<PropType> = ({ setting, setSetting, activity }) => {
+  const fetch = useSelector(rootSlice.load).fetch;
+
+  return !fetch && activity?.length ? (
     <button
       onClick={() => setSetting(!setting)}
       type="button"
@@ -24,5 +30,7 @@ export const Btn: React.FC<PropType> = ({ setting, setSetting }) => {
         />
       )}
     </button>
+  ) : (
+    <></>
   );
 };
