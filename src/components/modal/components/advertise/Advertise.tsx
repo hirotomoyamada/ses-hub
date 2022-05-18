278b9f39151ec98bef921f54411b2d24d3b80850
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2022-05-19 04:39:48 +0900

    add

--- src/components/modal/components/advertise/Advertise.tsx
+++ src/components/modal/components/advertise/Advertise.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 import styles from "./Advertise.module.scss";
 
-import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router";
-import * as userSlice from "features/user/userSlice";
-
 import { User } from "types/user";
+import { Active } from "./components/active/Active";
+import { Analytics } from "./components/analytics/Analytics";
 
 interface PropType {
   user: User;
-  text: string | undefined;
+  text?: string;
+  type?: string;
   handleClose: () => void;
   close?: () => void;
 }
@@ -17,69 +16,33 @@ interface PropType {
 export const Advertise: React.FC<PropType> = ({
   user,
   text,
+  type,
   close,
   handleClose,
 }) => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-
-  const handleOpen = (): void => {
-    navigate("/plan");
-    dispatch(userSlice.updateNotice());
-  };
-
   return (
     <div className={styles.advertise}>
-      <button
-        type="button"
-        className={styles.advertise_back}
-        onClick={!close ? handleClose : close}
-      >
-        今はしない
-      </button>
+      {(() => {
+        switch (type) {
+          case "active":
+            return (
+              <Active
+                user={user}
+                text={text}
+                handleClose={handleClose}
+                close={close}
+              />
+            );
 
-      <div
-        className={`${styles.advertise_header} ${
-          text && styles.advertise_header_account
-        }`}
-      >
-        {!text && (
-          <span className={styles.advertise_desc}>
-            {user?.payment?.trial
-              ? "\\ キャンペーン中 /"
-              : "\\ メンバーたちと一緒に /"}
-          </span>
-        )}
-        <p className={styles.advertise_ttl}>
-          {!text
-            ? user?.payment?.trial
-              ? "フリートライアル"
-              : "案件・人材を共有しませんか？"
-            : text}
-        </p>
-      </div>
+          case "analytics":
+            return (
+              <Analytics user={user} handleClose={handleClose} close={close} />
+            );
 
-      <button
-        type="button"
-        className={styles.advertise_btn}
-        onClick={handleOpen}
-      >
-        プランを見る
-      </button>
-
-      {user?.payment?.trial ? (
-        <img
-          src={`${process.env.PUBLIC_URL}/img/promotion/lets.svg`}
-          alt=""
-          className={styles.advertise_bg}
-        />
-      ) : (
-        <img
-          src={`${process.env.PUBLIC_URL}/img/app/advertise.svg`}
-          alt=""
-          className={`${styles.advertise_bg} ${styles.advertise_bg_people}`}
-        />
-      )}
+          default:
+            return <></>;
+        }
+      })()}
     </div>
   );
 };
