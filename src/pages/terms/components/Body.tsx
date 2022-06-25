bd073991fda7aae9e74580c0d42125c6e91875f0
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2022-06-25 19:05:35 +0900

    add

--- src/pages/terms/components/Body.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import styles from "../Terms.module.scss";
-import { body } from "../data/body";
-
-export const Body: React.FC = () => {
-  return <p className={styles.terms_body}>{body}</p>;
-};
