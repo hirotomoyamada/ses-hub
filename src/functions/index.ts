02cb0bad40ca8ff5fb3de33b99473a04019b3e35
Hirotomo Yamada <84060430+hirotomoyamada@users.noreply.github.com>
2022-06-25 19:04:07 +0900

    remove

--- src/functions/index.ts
+++
@@ -1,9 +0,0 @@
-export * as root from "./_root";
-export * as auth from "./_auth";
-export * as setting from "./_setting";
-export * as account from "./_account";
-export * as pay from "./_pay";
-export * as list from "./_list";
-export * as entry from "./_entry";
-export * as output from "./_output";
-export * as form from "./_form";
--- src/types/react-loader-spinner.d.ts
+++
@@ -1,7 +0,0 @@
-import { BaseProps as P } from "react-loader-spinner/dist/type";
-
-declare module "react-loader-spinner/dist/type" {
-  interface BaseProps extends P {
-    className?: string;
-  }
-}
