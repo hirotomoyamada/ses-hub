2711d7a64ca6b07469b00d44b63759f1300a71d3
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-01-09 16:35:12 +0900

    add 業界

--- src/functions/_entry.ts
+++ src/functions/_entry.ts
@@ -1,11 +1,12 @@
-import * as functions from "functions";
-import { Matter, Resource } from "types/post";
+import * as functions from 'functions';
+import { Matter, Resource } from 'types/post';
 
 export const matters = (post: Matter): string => {
   const value = {
     createAt: `作成：${functions.root.timestamp(post?.createAt)}\n`,
-    title: `■ ${post.title ? post.title : "不明な案件"}\n`,
-    positon: `${post?.position}\n`,
+    title: `■ ${post.title ? post.title : '不明な案件'}\n`,
+    position: `${post?.position}\n`,
+    industry: `業界：${post?.industry ?? '不明'}`,
     period: `開始：${
       post.period && `${post?.period?.year}年 ${post?.period?.month}月`
     }`,
@@ -16,7 +17,7 @@ export const matters = (post: Matter): string => {
     }`,
     adjustment: `精算：${post?.adjustment}\n`,
     costs: `単価：${
-      post?.costs?.display !== "public"
+      post?.costs?.display !== 'public'
         ? post?.costs?.type
         : post?.costs?.min
         ? `${post?.costs?.min}万 〜 ${post?.costs?.max}万`
@@ -27,12 +28,12 @@ export const matters = (post: Matter): string => {
       post?.interviews && `${post?.interviews?.type} ${post?.interviews?.count}`
     }`,
   };
-  return `${value.createAt}\n${value.title}\n${value.positon}\n${value.period}\n${value.location}\n${value.remote}\n${value.times}\n${value.adjustment}\n${value.costs}\n${value.distribution}\n${value.interviews}`;
+  return `${value.createAt}\n${value.title}\n${value.position}\n${value.industry}\n${value.period}\n${value.location}\n${value.remote}\n${value.times}\n${value.adjustment}\n${value.costs}\n${value.distribution}\n${value.interviews}`;
 };
 
 export const resources = (post: Resource): string => {
   const skills = post?.skills?.map((skill) => skill && `・${skill}`);
-  const skillsArray = skills && skills.join("\n");
+  const skillsArray = skills && skills.join('\n');
 
   const value = {
     createAt: `作成：${functions.root.timestamp(post?.createAt)}\n`,
@@ -40,11 +41,11 @@ export const resources = (post: Resource): string => {
       post?.roman
         ? `${post?.roman?.firstName.substring(
             0,
-            1
+            1,
           )} . ${post?.roman?.lastName.substring(0, 1)}`
-        : "不明な人材"
+        : '不明な人材'
     }`,
-    positon: `${post?.position}\n`,
+    position: `${post?.position}\n`,
     belong: `所属： ${post?.belong}`,
     sex: `性別： ${post?.sex}`,
     age: `年齢： ${post?.age}歳\n`,
@@ -53,7 +54,7 @@ export const resources = (post: Resource): string => {
     }`,
     station: `最寄：${post?.station}\n`,
     costs: `単価：${
-      post?.costs?.display !== "public"
+      post?.costs?.display !== 'public'
         ? post?.costs?.type
         : post?.costs?.min
         ? `${post?.costs?.min}万 〜 ${post?.costs?.max}万`
@@ -62,5 +63,5 @@ export const resources = (post: Resource): string => {
     skills: `スキル：\n${skillsArray}\n`,
   };
 
-  return `${value.createAt}\n${value.roman}\n${value.positon}\n${value.belong}\n${value.sex}\n${value.age}\n${value.period}\n${value.station}\n${value.costs}\n${value.skills}\n`;
+  return `${value.createAt}\n${value.roman}\n${value.position}\n${value.belong}\n${value.sex}\n${value.age}\n${value.period}\n${value.station}\n${value.costs}\n${value.skills}\n`;
 };
