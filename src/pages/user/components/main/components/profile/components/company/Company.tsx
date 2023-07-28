2f18aff0a48e3dbf6730cd4b3557e5d423bd88ca
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2023-07-29 08:27:33 +0900

    fix: hidden tel, email, sns

--- src/pages/user/components/main/components/profile/components/company/Company.tsx
+++ src/pages/user/components/main/components/profile/components/company/Company.tsx
@@ -1,20 +1,17 @@
-import React from "react";
-import styles from "./Company.module.scss";
+import React from 'react';
+import styles from './Company.module.scss';
 
-import { Head } from "./components/Head";
-import { Body } from "./components/Body";
-import { More } from "./components/More";
-import { Address } from "./components/Address";
-import { Tel } from "./components/Tel";
-import { Email } from "./components/Email";
-import { Url } from "./components/Url";
-import { CreateAt } from "./components/CreateAt";
-import { Social } from "./components/Social";
-import { Payment } from "./components/Payment";
+import { Head } from './components/Head';
+import { Body } from './components/Body';
+import { More } from './components/More';
+import { Address } from './components/Address';
+import { Url } from './components/Url';
+import { CreateAt } from './components/CreateAt';
+import { Payment } from './components/Payment';
 
-import * as Post from "types/post";
-import { User } from "types/user";
-import { Follow } from "./components/Follow";
+import * as Post from 'types/post';
+import { User } from 'types/user';
+import { Follow } from './components/Follow';
 
 interface PropType {
   user: Post.Company;
@@ -35,17 +32,9 @@ export const Company: React.FC<PropType> = ({ user, demo }) => {
 
         <Address user={user} demo={demo} />
 
-        <div className={styles.profile_wrap}>
-          <Tel user={user} demo={demo} />
-
-          <Email user={user} demo={demo} />
-        </div>
-
         <Url user={user} demo={demo} />
 
         <CreateAt user={user} />
-
-        <Social user={user} demo={demo} />
 
         <Follow user={user as Post.Company | User} />
       </div>
