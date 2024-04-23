95e04553c80ffabaeebb8bb0bf3e9d5b0d723028
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-04-23 17:09:21 +0900

    style(modal): updated color variables in scss files

--- src/pages/post/Meta.tsx
+++ src/pages/post/Meta.tsx
@@ -1,33 +1,30 @@
-import React from "react";
-import { Helmet } from "react-helmet-async";
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
 
-import { Matter, Resource } from "types/post";
+import { Matter, Resource } from 'types/post';
 
 interface PropType {
-  index: "matters" | "resources";
+  index: 'matters' | 'resources';
   post: Matter | Resource;
 }
 
 export const Meta: React.FC<PropType> = ({ index, post }) => {
   return (
     <Helmet>
-      {index === "matters" ? (
+      {index === 'matters' ? (
         <title>
-          {(post as Matter)?.title
-            ? `${(post as Matter).title}｜SES_HUB`
-            : "SES_HUB｜営業支援App"}
+          {(post as Matter)?.title ? `${(post as Matter).title}｜SES_HUB` : 'SES_HUB｜営業支援App'}
         </title>
-      ) : index === "resources" ? (
+      ) : index === 'resources' ? (
         <title>
-          {(post as Resource)?.roman?.firstName &&
-          (post as Resource)?.roman?.lastName
-            ? `${(post as Resource).roman.firstName} . ${
-                (post as Resource).roman.lastName
-              }｜SES_HUB`
-            : "SES_HUB｜営業支援App"}
+          {(post as Resource)?.roman?.firstName && (post as Resource)?.roman?.lastName
+            ? `${(post as Resource).roman.firstName.slice(0, 1)} . ${(
+                post as Resource
+              ).roman.lastName.slice(0, 1)}｜SES_HUB`
+            : 'SES_HUB｜営業支援App'}
         </title>
       ) : (
-        <title>{"SES_HUB｜営業支援App"}</title>
+        <title>{'SES_HUB｜営業支援App'}</title>
       )}
     </Helmet>
   );
