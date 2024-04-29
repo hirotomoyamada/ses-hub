d544d0ccfc2641a19027753b5ba50c778585374e
hirotomoyamada <hirotomo.yamada@avap.co.jp>
2024-04-29 15:28:47 +0900

    feat(Entries): added modal and proposal options in UI

--- src/pages/howTo/components/Main/Entries.tsx
+++ src/pages/howTo/components/Main/Entries.tsx
@@ -17,9 +17,31 @@ export const Entries: React.FC = () => {
           他のメンバーが投稿した
           <span className={styles.howto_container_tag}>案件</span>
           <span className={styles.howto_container_tag}>人材</span>
-          に問い合わせることが可能です。投稿の詳細から、
-          <span className={styles.howto_container_tag}>問い合わせをする</span>
-          を選択すると、メールや投稿したメンバーのSNSに直接問い合わせすることができます。
+          に問い合わせることが可能です。
+          <br />
+          <br />
+          投稿の詳細から、<span className={styles.howto_container_tag}>問い合わせをする</span>
+          を選択すると、メンバーに問い合わせできるモーダルが表示されます。
+        </p>
+      </div>
+
+      <div className={styles.howto_container}>
+        <Tag tag='️提案したい案件・人材' paid />
+
+        <p>
+          他のメンバーの
+          <span className={styles.howto_container_tag}>案件</span>
+          <span className={styles.howto_container_tag}>人材</span>
+          に対し、
+          <br />
+          <br />
+          <span className={styles.howto_container_acnt}>ご自身が登録した案件・人材の中から、</span>
+          <br />
+          <br />
+          提案したい
+          <span className={styles.howto_container_tag}>案件</span>
+          <span className={styles.howto_container_tag}>人材</span>
+          を選択し、問い合わせすることができます。
         </p>
       </div>
 
